cafd38a0c55609df7fea52a0d949092261b72a7f
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-18 13:22:44 +0900

    rawmidi: add global method to get information of substream for rawmidi device
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/rawmidi/privates.h
+++ src/rawmidi/privates.h
@@ -7,6 +7,10 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include "substream-info.h"
+
+#include <sound/asound.h>
+
 G_BEGIN_DECLS
 
 GQuark alsarawmidi_error_quark(void);
@@ -14,6 +18,9 @@ GQuark alsarawmidi_error_quark(void);
 #define generate_error(err, errno)                              \
     g_set_error(err, alsarawmidi_error_quark(), errno,          \
                 __FILE__ ":%d: %s", __LINE__, strerror(errno))
+
+void rawmidi_substream_info_refer_private(ALSARawmidiSubstreamInfo *self,
+                                          struct snd_rawmidi_info **info);
 
 G_END_DECLS
 
