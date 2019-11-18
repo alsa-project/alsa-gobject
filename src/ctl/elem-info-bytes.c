a12eb9b1ac5c5b614c2448993f021bf43a8be254
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-18 13:22:44 +0900

    ctl: elem_info_bytes: add object to represent information for element of bytes type

---
+++ src/ctl/elem-info-bytes.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "elem-info-bytes.h"
+
+G_DEFINE_TYPE(ALSACtlElemInfoBytes, alsactl_elem_info_bytes, ALSACTL_TYPE_ELEM_INFO)
+
+static void alsactl_elem_info_bytes_class_init(ALSACtlElemInfoBytesClass *klass)
+{
+    return;
+}
+
+static void alsactl_elem_info_bytes_init(ALSACtlElemInfoBytes *self)
+{
+    return;
+}
