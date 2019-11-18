4472003afcfd4cd223ee48368cf238084737f533
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-18 13:22:44 +0900

    ctl: elem_info_bool: add object to represent information for boolean type element

---
+++ src/ctl/elem-info-bool.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "elem-info-bool.h"
+
+G_DEFINE_TYPE(ALSACtlElemInfoBool, alsactl_elem_info_bool, ALSACTL_TYPE_ELEM_INFO)
+
+static void alsactl_elem_info_bool_class_init(ALSACtlElemInfoBoolClass *klass)
+{
+    return;
+}
+
+static void alsactl_elem_info_bool_init(ALSACtlElemInfoBool *self)
+{
+    return;
+}
+
