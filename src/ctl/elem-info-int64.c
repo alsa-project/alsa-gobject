c8761a6aa34e8165183cff77c92a815356c30a1c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-18 13:22:44 +0900

    ctl: elem_info_int64: add object to represent information for integer64 type element

---
+++ src/ctl/elem-info-int64.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "elem-info-int64.h"
+
+G_DEFINE_TYPE(ALSACtlElemInfoInt64, alsactl_elem_info_int64, ALSACTL_TYPE_ELEM_INFO)
+
+static void alsactl_elem_info_int64_class_init(ALSACtlElemInfoInt64Class *klass)
+{
+    return;
+}
+
+static void alsactl_elem_info_int64_init(ALSACtlElemInfoInt64 *self)
+{
+    return;
+}
