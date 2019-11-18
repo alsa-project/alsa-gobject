0805c4f7c8b3bb82bbde18f4e63b6df31cfb286b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-18 13:22:44 +0900

    ctl: elem_info: add object to represent information of element

---
+++ src/ctl/elem-info.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "elem-info.h"
+
+G_DEFINE_ABSTRACT_TYPE(ALSACtlElemInfo, alsactl_elem_info, G_TYPE_OBJECT)
+
+static void alsactl_elem_info_class_init(ALSACtlElemInfoClass *klass)
+{
+    return;
+}
+
+static void alsactl_elem_info_init(ALSACtlElemInfo *self)
+{
+    return;
+}
---
+++ src/ctl/elem-info.h
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSACTL_ELEM_INFO__H__
+#define __ALSA_GOBJECT_ALSACTL_ELEM_INFO__H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define ALSACTL_TYPE_ELEM_INFO      (alsactl_elem_info_get_type())
+
+#define ALSACTL_ELEM_INFO(obj)                              \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj),                      \
+                                ALSACTL_TYPE_ELEM_INFO,     \
+                                ALSACtlElemInfo))
+#define ALSACTL_IS_ELEM_INFO(obj)                           \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                      \
+                                ALSACTL_TYPE_ELEM_INFO))
+
+#define ALSACTL_ELEM_INFO_CLASS(klass)                      \
+    (G_TYPE_CHECK_CLASS_CAST((klass),                       \
+                             ALSACTL_TYPE_ELEM_INFO,        \
+                             ALSACtlElemInfoClass))
+#define ALSACTL_IS_ELEM_INFO_CLASS(klass)                   \
+    (G_TYPE_CHECK_CLASS_TYPE((klass),                       \
+                             ALSACTL_TYPE_ELEM_INFO))
+#define ALSACTL_ELEM_INFO_GET_CLASS(obj)                    \
+    (G_TYPE_INSTANCE_GET_CLASS((obj),                       \
+                               ALSACTL_TYPE_ELEM_INFO,      \
+                               ALSACtlElemInfoClass))
+
+typedef struct _ALSACtlElemInfo         ALSACtlElemInfo;
+typedef struct _ALSACtlElemInfoClass    ALSACtlElemInfoClass;
+
+struct _ALSACtlElemInfo {
+    GObject parent_instance;
+};
+
+struct _ALSACtlElemInfoClass {
+    GObjectClass parent_class;
+};
+
+GType alsactl_elem_info_get_type() G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif
