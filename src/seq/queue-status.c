386d741192314a8ae1c47782f41afd3495fb93ac
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-20 18:17:14 +0900

    seq: queue_status: add object to represent status of queue
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/queue-status.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "queue-status.h"
+
+G_DEFINE_TYPE(ALSASeqQueueStatus, alsaseq_queue_status, G_TYPE_OBJECT)
+
+static void alsaseq_queue_status_class_init(ALSASeqQueueStatusClass *klass)
+{
+}
+
+static void alsaseq_queue_status_init(ALSASeqQueueStatus *self)
+{
+    return;
+}
