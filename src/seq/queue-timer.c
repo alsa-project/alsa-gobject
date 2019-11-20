4018fe02a4eaa1534e66d952ae0cb0402a50dfe6
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-20 22:10:39 +0900

    seq: queue_timer: add abstract object to represent timer attached to queue
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/queue-timer.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "queue-timer.h"
+
+G_DEFINE_ABSTRACT_TYPE(ALSASeqQueueTimer, alsaseq_queue_timer, G_TYPE_OBJECT)
+
+static void alsaseq_queue_timer_class_init(ALSASeqQueueTimerClass *klass)
+{
+    return;
+}
+
+static void alsaseq_queue_timer_init(ALSASeqQueueTimer *self)
+{
+    return;
+}
---
+++ src/seq/queue-timer.h
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSASEQ_QUEUE_TIMER__H__
+#define __ALSA_GOBJECT_ALSASEQ_QUEUE_TIMER__H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define ALSASEQ_TYPE_QUEUE_TIMER     (alsaseq_queue_timer_get_type())
+
+#define ALSASEQ_QUEUE_TIMER(obj)                            \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj),                      \
+                                ALSASEQ_TYPE_QUEUE_TIMER,   \
+                                ALSASeqQueueTimer))
+#define ALSASEQ_IS_QUEUE_TIMER(obj)                         \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                      \
+                                ALSASEQ_TYPE_QUEUE_TIMER))
+
+#define ALSASEQ_QUEUE_TIMER_CLASS(klass)                    \
+    (G_TYPE_CHECK_CLASS_CAST((klass),                       \
+                             ALSASEQ_TYPE_QUEUE_TIMER,      \
+                             ALSASeqQueueTimerClass))
+#define ALSASEQ_IS_QUEUE_TIMER_CLASS(klass)                 \
+    (G_TYPE_CHECK_CLASS_TYPE((klass),                       \
+                             ALSASEQ_TYPE_QUEUE_TIMER))
+#define ALSASEQ_QUEUE_TIMER_GET_CLASS(obj)                  \
+    (G_TYPE_INSTANCE_GET_CLASS((obj),                       \
+                               ALSASEQ_TYPE_QUEUE_TIMER,    \
+                               ALSASeqQueueTimerClass))
+
+typedef struct _ALSASeqQueueTimer           ALSASeqQueueTimer;
+typedef struct _ALSASeqQueueTimerClass      ALSASeqQueueTimerClass;
+
+struct _ALSASeqQueueTimer {
+    GObject parent_instance;
+};
+
+struct _ALSASeqQueueTimerClass {
+    GObjectClass parent_class;
+};
+
+GType alsaseq_queue_timer_get_type() G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif
