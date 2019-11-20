ebbf8f12b2519c10b7785b282d8b1939822dc4f6
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-20 22:29:10 +0900

    seq: queue_timer_alsa: add object to represent queue-timer specialized to ALSA Timer functionality
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/queue-timer-alsa.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "queue-timer-alsa.h"
+
+G_DEFINE_TYPE(ALSASeqQueueTimerAlsa, alsaseq_queue_timer_alsa, ALSASEQ_TYPE_QUEUE_TIMER)
+
+static void alsaseq_queue_timer_alsa_class_init(ALSASeqQueueTimerAlsaClass *klass)
+{
+    return;
+}
+
+static void alsaseq_queue_timer_alsa_init(ALSASeqQueueTimerAlsa *self)
+{
+    return;
+}
