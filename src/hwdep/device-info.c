969c9afbcce76235e9404bb958cde8672d1ca4fa
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-11-27 15:44:05 +0900

    hwdep: device_info: add object to represent information of hwdep device
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/hwdep/device-info.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "device-info.h"
+
+G_DEFINE_TYPE(ALSAHwdepDeviceInfo, alsahwdep_device_info, G_TYPE_OBJECT)
+
+static void alsahwdep_device_info_class_init(ALSAHwdepDeviceInfoClass *klass)
+{
+    return;
+}
+
+static void alsahwdep_device_info_init(ALSAHwdepDeviceInfo *self)
+{
+    return;
+}
---
+++ src/hwdep/device-info.h
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSAHWDEP_DEVICE_INFO__H__
+#define __ALSA_GOBJECT_ALSAHWDEP_DEVICE_INFO__H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define ALSAHWDEP_TYPE_DEVICE_INFO      (alsahwdep_device_info_get_type())
+
+#define ALSAHWDEP_DEVICE_INFO(obj)                          \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj),                      \
+                                ALSAHWDEP_TYPE_DEVICE_INFO, \
+                                ALSAHwdepDeviceInfo))
+#define ALSAHWDEP_IS_DEVICE_INFO(obj)                       \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                      \
+                                ALSAHWDEP_TYPE_DEVICE_INFO))
+
+#define ALSAHWDEP_DEVICE_INFO_CLASS(klass)                  \
+    (G_TYPE_CHECK_CLASS_CAST((klass),                       \
+                             ALSAHWDEP_TYPE_DEVICE_INFO,    \
+                             ALSAHwdepDeviceInfoClass))
+#define ALSAHWDEP_IS_DEVICE_INFO_CLASS(klass)               \
+    (G_TYPE_CHECK_CLASS_TYPE((klass),                       \
+                             ALSAHWDEP_TYPE_DEVICE_INFO))
+#define ALSAHWDEP_DEVICE_INFO_GET_CLASS(obj)                \
+    (G_TYPE_INSTANCE_GET_CLASS((obj),                       \
+                               ALSAHWDEP_TYPE_DEVICE_INFO,  \
+                               ALSAHwdepDeviceInfoClass))
+
+typedef struct _ALSAHwdepDeviceInfo         ALSAHwdepDeviceInfo;
+typedef struct _ALSAHwdepDeviceInfoClass    ALSAHwdepDeviceInfoClass;
+
+struct _ALSAHwdepDeviceInfo {
+    GObject parent_instance;
+};
+
+struct _ALSAHwdepDeviceInfoClass {
+    GObjectClass parent_class;
+};
+
+GType alsahwdep_device_info_get_type() G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif
