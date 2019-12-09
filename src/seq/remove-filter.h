562c94b86b4af67635b11911d36f979d9946c46d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-12-09 22:56:39 +0900

    seq: remove_filter; add constructor methods
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/remove-filter.h
+++ src/seq/remove-filter.h
@@ -5,6 +5,9 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include <seq/addr.h>
+#include <seq/alsaseq-enums.h>
+
 #include <sound/asequencer.h>
 
 G_BEGIN_DECLS
@@ -14,6 +17,36 @@ G_BEGIN_DECLS
 typedef struct snd_seq_remove_events    ALSASeqRemoveFilter;
 
 GType alsaseq_remove_filter_get_type() G_GNUC_CONST;
+
+ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_dest_addr(
+                                ALSASeqRemoveFilterFlag inout, guint8 queue_id,
+                                ALSASeqAddr *dest, GError **error);
+
+ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_note_channel(
+                                ALSASeqRemoveFilterFlag inout, guint8 queue_id,
+                                guint8 channel, GError **error);
+
+ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_event_type(
+                                ALSASeqRemoveFilterFlag inout, guint8 queue_id,
+                                ALSASeqEventType ev_type, GError **error);
+
+ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_note(
+                                ALSASeqRemoveFilterFlag inout, guint8 queue_id,
+                                GError **error);
+
+ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_tag(
+                                ALSASeqRemoveFilterFlag inout, guint8 queue_id,
+                                gint8 tag, GError **error);
+
+ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_tick_time(
+                                ALSASeqRemoveFilterFlag inout, guint8 queue_id,
+                                gint32 tick_time, gboolean after,
+				GError **error);
+
+ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_real_time(
+                                ALSASeqRemoveFilterFlag inout, guint8 queue_id,
+                                gint32 tv_sec, guint32 tv_nsec, gboolean after,
+                                GError **error);
 
 G_END_DECLS
 
