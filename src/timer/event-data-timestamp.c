3e1fcbcd1de74c047866bb68acef8cd63f085116
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-02-09 12:20:53 +0900

    timer: event_timestamp: add derived object for timestamp event
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/timer/event-data-timestamp.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "event-data-timestamp.h"
+
+G_DEFINE_TYPE(ALSATimerEventDataTimestamp, alsatimer_event_data_timestamp, ALSATIMER_TYPE_EVENT_DATA)
+
+static void alsatimer_event_data_timestamp_class_init(ALSATimerEventDataTimestampClass *klass)
+{
+}
+
+static void alsatimer_event_data_timestamp_init(ALSATimerEventDataTimestamp *self)
+{
+    return;
+}
