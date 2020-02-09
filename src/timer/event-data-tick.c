414e3f4eae9324e1299636a9c431f3b05cfd03de
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-02-09 12:20:53 +0900

    timer: event_tick: add derived object to represent event with tick count
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/timer/event-data-tick.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "event-data-tick.h"
+
+G_DEFINE_TYPE(ALSATimerEventDataTick, alsatimer_event_data_tick, ALSATIMER_TYPE_EVENT_DATA)
+
+static void alsatimer_event_data_tick_class_init(ALSATimerEventDataTickClass *klass)
+{
+}
+
+static void alsatimer_event_data_tick_init(ALSATimerEventDataTick *self)
+{
+    return;
+}
+
