19ccc8bcea8c1ae380ad6a46a788831ae1793dd9
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-02-09 12:20:53 +0900

    timer: instance_status: add object to represent status of instance
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/timer/instance-status.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "instance-status.h"
+
+G_DEFINE_TYPE(ALSATimerInstanceStatus, alsatimer_instance_status, G_TYPE_OBJECT)
+
+static void alsatimer_instance_status_class_init(ALSATimerInstanceStatusClass *klass)
+{
+}
+
+static void alsatimer_instance_status_init(ALSATimerInstanceStatus *self)
+{
+    return;
+}
