ceec0a84f54fba73fe3c8d9f3c1371a15c88e453
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-02-09 12:20:53 +0900

    timer: instance_info: add object to represent information of instance
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/timer/instance-info.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "instance-info.h"
+
+G_DEFINE_TYPE(ALSATimerInstanceInfo, alsatimer_instance_info, G_TYPE_OBJECT)
+
+static void alsatimer_instance_info_class_init(ALSATimerInstanceInfoClass *klass)
+{
+    return;
+}
+
+static void alsatimer_instance_info_init(ALSATimerInstanceInfo *self)
+{
+    return;
+}
