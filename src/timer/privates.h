6e323301d989b387e332c2c47ffe7a4722ee169b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-02-09 12:20:53 +0900

    timer: add Quark object to report any error
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/timer/privates.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSATIMER_PRIVATES__H__
+#define __ALSA_GOBJECT_ALSATIMER_PRIVATES__H__
+
+G_BEGIN_DECLS
+
+GQuark alsatimer_error_quark(void);
+
+#define generate_error(err, errno)                              \
+    g_set_error(err, alsatimer_error_quark(), errno,            \
+                __FILE__ ":%d: %s", __LINE__, strerror(errno))
+
+G_END_DECLS
+
+#endif
