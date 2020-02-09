c5e0d6dd29bd19e1d7cc508306bbbd1869c772b2
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-02-09 12:20:53 +0900

    timer: event_timestamp: add derived object for timestamp event
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/timer/event-data-timestamp.h
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSATIMER_EVENT_DATA_TIMESTAMP__H__
+#define __ALSA_GOBJECT_ALSATIMER_EVENT_DATA_TIMESTAMP__H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <timer/event-data.h>
+
+G_BEGIN_DECLS
+
+#define ALSATIMER_TYPE_EVENT_DATA_TIMESTAMP     (alsatimer_event_data_timestamp_get_type())
+
+#define ALSATIMER_EVENT_DATA_TIMESTAMP(obj)                             \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj),                                  \
+                                ALSATIMER_TYPE_EVENT_DATA_TIMESTAMP,    \
+                                ALSATimerEventDataTimestamp))
+#define ALSATIMER_IS_EVENT_DATA_TIMESTAMP(obj)                          \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                                  \
+                                ALSATIMER_TYPE_EVENT_DATA_TIMESTAMP))
+
+#define ALSATIMER_EVENT_DATA_TIMESTAMP_CLASS(klass)                     \
+    (G_TYPE_CHECK_CLASS_CAST((klass),                                   \
+                             ALSATIMER_TYPE_EVENT_DATA_TIMESTAMP,       \
+                             ALSATimerEventDataTimestampClass))
+#define ALSATIMER_IS_EVENT_DATA_TIMESTAMP_CLASS(klass)                  \
+    (G_TYPE_CHECK_CLASS_TYPE((klass),                                   \
+                             ALSATIMER_TYPE_EVENT_DATA_TIMESTAMP))
+#define ALSATIMER_EVENT_DATA_TIMESTAMP_GET_CLASS(obj)                   \
+    (G_TYPE_INSTANCE_GET_CLASS((obj),                                   \
+                               ALSATIMER_TYPE_EVENT_DATA_TIMESTAMP,     \
+                               ALSATimerEventDataTimestampkClass))
+
+typedef struct _ALSATimerEventDataTimestamp         ALSATimerEventDataTimestamp;
+typedef struct _ALSATimerEventDataTimestampClass    ALSATimerEventDataTimestampClass;
+
+struct _ALSATimerEventDataTimestamp {
+    ALSATimerEventData parent_instance;
+};
+
+struct _ALSATimerEventDataTimestampClass {
+    ALSATimerEventDataClass parent_class;
+};
+
+GType alsatimer_event_data_timestamp_get_type() G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif
