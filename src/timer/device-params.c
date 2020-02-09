e239969a63896f6b3b398632cecf5bd6d44d7c01
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-02-09 12:20:53 +0900

    timer: device_params: add object to represent parameters of timer device
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/timer/device-params.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "device-params.h"
+
+G_DEFINE_TYPE(ALSATimerDeviceParams, alsatimer_device_params, G_TYPE_OBJECT)
+
+static void alsatimer_device_params_class_init(ALSATimerDeviceParamsClass *klass)
+{
+    return;
+}
+
+static void alsatimer_device_params_init(ALSATimerDeviceParams *self)
+{
+    return;
+}
