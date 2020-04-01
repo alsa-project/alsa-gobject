abf7d7385ab5f6bbe4c17c21aeccc44eaa9f330b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: event_fixed: add GObject object for event with fixed length data
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/event-fixed.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "event-fixed.h"
+
+G_DEFINE_TYPE(ALSASeqEventFixed, alsaseq_event_fixed, ALSASEQ_TYPE_EVENT)
+
+static void alsaseq_event_fixed_class_init(ALSASeqEventFixedClass *klass)
+{
+    return;
+}
+
+static void alsaseq_event_fixed_init(ALSASeqEventFixed *self)
+{
+    return;
+}
