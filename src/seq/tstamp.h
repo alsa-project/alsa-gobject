478564ba0bdd22f6a589fee749659ec6fa3fa0aa
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: tstamp: add accessor methods
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/tstamp.h
+++ src/seq/tstamp.h
@@ -15,6 +15,12 @@ typedef union snd_seq_timestamp ALSASeqTstamp;
 
 GType alsaseq_tstamp_get_type() G_GNUC_CONST;
 
+void alsaseq_tstamp_get_tick_time(ALSASeqTstamp *self, guint32 *tick_time);
+void alsaseq_tstamp_set_tick_time(ALSASeqTstamp *self, const guint32 tick_time);
+
+const guint32 *alsaseq_tstamp_get_real_time(ALSASeqTstamp *self);
+void alsaseq_tstamp_set_real_time(ALSASeqTstamp *self, const guint32 tstamp[2]);
+
 G_END_DECLS
 
 #endif
