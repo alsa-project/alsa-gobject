d714ba87427fb4dd3a05e84f0f73edb8212d2e8e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: event_data_result: add accessor methods
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/event-data-result.c
+++ src/seq/event-data-result.c
@@ -7,3 +7,55 @@ ALSASeqEventDataResult *seq_event_data_result_copy(const ALSASeqEventDataResult 
 }
 
 G_DEFINE_BOXED_TYPE(ALSASeqEventDataResult, alsaseq_event_data_result, seq_event_data_result_copy, g_free)
+
+/**
+ * alsaseq_event_data_result_get_event:
+ * @self: A #ALSASeqEventDataResult.
+ *
+ * Get the type of event in which the data results.
+ *
+ * Returns: The type of event in which the data results.
+ */
+ALSASeqEventType alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self)
+{
+    return (ALSASeqEventType)self->event;
+}
+
+/**
+ * alsaseq_event_data_result_set_event:
+ * @self: A #ALSASeqEventDataResult.
+ * @event_type: A #ALSASeqEventType.
+ *
+ * Set the type of event in which the data results.
+ */
+void alsaseq_event_data_result_set_event(ALSASeqEventDataResult *self,
+                                         ALSASeqEventType event_type)
+{
+    self->event = (int)event_type;
+}
+
+/**
+ * alsaseq_event_data_result_get_result:
+ * @self: A #ALSASeqEventDataResult.
+ *
+ * Get the status of event.
+ *
+ * Returns: the status of the event.
+ */
+gint alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self)
+{
+    return self->result;
+}
+
+/**
+ * alsaseq_event_data_result_set_result:
+ * @self: A #ALSASeqEventDataResult.
+ * @result: The status of event.
+ *
+ * Set the status of event.
+ */
+void alsaseq_event_data_result_set_result(ALSASeqEventDataResult *self,
+                                          gint result)
+{
+    self->result = result;
+}
