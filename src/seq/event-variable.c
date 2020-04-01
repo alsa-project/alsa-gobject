c6df481747810b2596606c5d8b87d9d7ef78bbff
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: event_variable: add GObject object for event with variable length data
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/event-variable.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "event-variable.h"
+
+G_DEFINE_TYPE(ALSASeqEventVariable, alsaseq_event_variable, ALSASEQ_TYPE_EVENT)
+
+static void alsaseq_event_variable_class_init(ALSASeqEventVariableClass *klass)
+{
+    return;
+}
+
+static void alsaseq_event_variable_init(ALSASeqEventVariable *self)
+{
+    return;
+}
