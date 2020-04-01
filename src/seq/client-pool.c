8aea841808ea7493338831385b0215f7253f27de
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: client_pool: add object to represent memory pool in client
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/client-pool.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "client-pool.h"
+
+G_DEFINE_TYPE(ALSASeqClientPool, alsaseq_client_pool, G_TYPE_OBJECT)
+
+static void alsaseq_client_pool_class_init(ALSASeqClientPoolClass *klass)
+{
+    return;
+}
+
+static void alsaseq_client_pool_init(ALSASeqClientPool *self)
+{
+    return;
+}
