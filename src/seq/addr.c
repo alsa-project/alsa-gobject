6d52aed50e3fd3d45f285b2f4d1b355ecf9e0731
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: addr: add accessor methods and constructors
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/addr.c
+++ src/seq/addr.c
@@ -7,3 +7,46 @@ ALSASeqAddr *seq_addr_copy(const ALSASeqAddr *self)
 }
 
 G_DEFINE_BOXED_TYPE(ALSASeqAddr, alsaseq_addr, seq_addr_copy, g_free)
+
+/**
+ * alsaseq_addr_new:
+ * @client_id: The numerical ID of client to address.
+ * @port_id: The numerical ID of port to address.
+ *
+ * Allocate and return an instance of ALSASeqAddr.
+ *
+ * Returns: (transfer full): A #ALSASeqAddr.
+ */
+ALSASeqAddr *alsaseq_addr_new(guint8 client_id, guint8 port_id)
+{
+    ALSASeqAddr *self = g_malloc0(sizeof(*self));
+
+    self->client = client_id;
+    self->port = port_id;
+
+    return self;
+}
+
+/**
+ * alsaseq_addr_get_client_id:
+ * @self: A #ALSASeqAddr.
+ * @client_id: (out): The numerical ID of client to address.
+ *
+ * Get the numerical ID of client to address.
+ */
+void alsaseq_addr_get_client_id(const ALSASeqAddr *self, guint8 *client_id)
+{
+    *client_id = self->client;
+}
+
+/**
+ * alsaseq_addr_get_port_id:
+ * @self: A #ALSASeqAddr.
+ * @port_id: (out): The numerical ID of port to address.
+ *
+ * Get the numerical ID of port to address.
+ */
+void alsaseq_addr_get_port_id(const ALSASeqAddr *self, guint8 *port_id)
+{
+    *port_id = self->port;
+}
