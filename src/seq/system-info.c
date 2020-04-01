7fffb01cc8ef0be2708ac8e6b2ab89040cb0f1eb
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: system_info: add object to represent information of ALSA sequencer
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/system-info.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "system-info.h"
+
+G_DEFINE_TYPE(ALSASeqSystemInfo, alsaseq_system_info, G_TYPE_OBJECT)
+
+static void alsaseq_system_info_class_init(ALSASeqSystemInfoClass *klass)
+{
+    return;
+}
+
+static void alsaseq_system_info_init(ALSASeqSystemInfo *self)
+{
+    return;
+}
---
+++ src/seq/system-info.h
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSASEQ_SYSTEM_INFO__H__
+#define __ALSA_GOBJECT_ALSASEQ_SYSTEM_INFO__H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define ALSASEQ_TYPE_SYSTEM_INFO     (alsaseq_system_info_get_type())
+
+#define ALSASEQ_SYSTEM_INFO(obj)                            \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj),                      \
+                                ALSASEQ_TYPE_SYSTEM_INFO,   \
+                                ALSASeqSystemInfo))
+#define ALSASEQ_IS_SYSTEM_INFO(obj)                         \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                      \
+                                ALSASEQ_TYPE_SYSTEM_INFO))
+
+#define ALSASEQ_SYSTEM_INFO_CLASS(klass)                    \
+    (G_TYPE_CHECK_CLASS_CAST((klass),                       \
+                             ALSASEQ_TYPE_SYSTEM_INFO,      \
+                             ALSASeqSystemInfoClass))
+#define ALSASEQ_IS_SYSTEM_INFO_CLASS(klass)                 \
+    (G_TYPE_CHECK_CLASS_TYPE((klass),                       \
+                             ALSASEQ_TYPE_SYSTEM_INFO))
+#define ALSASEQ_SYSTEM_INFO_GET_CLASS(obj)                  \
+    (G_TYPE_INSTANCE_GET_CLASS((obj),                       \
+                               ALSASEQ_TYPE_SYSTEM_INFO,    \
+                               ALSASeqSystemInfoClass))
+
+typedef struct _ALSASeqSystemInfo           ALSASeqSystemInfo;
+typedef struct _ALSASeqSystemInfoClass      ALSASeqSystemInfoClass;
+
+struct _ALSASeqSystemInfo {
+    GObject parent_instance;
+};
+
+struct _ALSASeqSystemInfoClass {
+    GObjectClass parent_class;
+};
+
+GType alsaseq_system_info_get_type() G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif
