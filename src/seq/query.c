1626987d88d30fdfe0b2c98d95b6323c22d265e4
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: add global method to get devnode string for ALSA Sequencer
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/query.c
+++ src/seq/query.c
@@ -52,3 +52,45 @@ void alsaseq_get_seq_sysname(gchar **sysname, GError **error)
     udev_device_unref(dev);
     udev_unref(ctx);
 }
+
+/**
+ * alsaseq_get_seq_devnode:
+ * @devnode: (out): The devnode of ALSA Sequencer.
+ * @error: A #GError.
+ *
+ * Allocate devnode string for ALSA Sequencer and return it when exists.
+ */
+void alsaseq_get_seq_devnode(gchar **devnode, GError **error)
+{
+    struct udev *ctx;
+    struct udev_device *dev;
+    const char *node;
+
+    ctx = udev_new();
+    if (ctx == NULL) {
+        generate_error(error, errno);
+        return;
+    }
+
+    dev = udev_device_new_from_subsystem_sysname(ctx, "sound", SEQ_SYSNAME);
+    if (dev == NULL) {
+        generate_error(error, ENODEV);
+        udev_unref(ctx);
+        return;
+    }
+
+    node = udev_device_get_devnode(dev);
+    if (node == NULL) {
+        generate_error(error, ENODEV);
+        udev_device_unref(dev);
+        udev_unref(ctx);
+        return;
+    }
+
+    *devnode = strdup(node);
+    if (*devnode == NULL)
+        generate_error(error, ENOMEM);
+
+    udev_device_unref(dev);
+    udev_unref(ctx);
+}
