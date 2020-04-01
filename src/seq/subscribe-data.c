ade2dd592898ea666d524c2c14023334c35d34a0
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: subscribe_data: add object to represent subscription between two ports
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/subscribe-data.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "subscribe-data.h"
+
+G_DEFINE_TYPE(ALSASeqSubscribeData, alsaseq_subscribe_data, G_TYPE_OBJECT)
+
+static void alsaseq_subscribe_data_class_init(ALSASeqSubscribeDataClass *klass)
+{
+    return;
+}
+
+static void alsaseq_subscribe_data_init(ALSASeqSubscribeData *self)
+{
+    return;
+}
---
+++ src/seq/subscribe-data.h
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSASEQ_SUBSCRIBE_DATA__H__
+#define __ALSA_GOBJECT_ALSASEQ_SUBSCRIBE_DATA__H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define ALSASEQ_TYPE_SUBSCRIBE_DATA     (alsaseq_subscribe_data_get_type())
+
+#define ALSASEQ_SUBSCRIBE_DATA(obj)                             \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj),                          \
+                                ALSASEQ_TYPE_SUBSCRIBE_DATA,    \
+                                ALSASeqSubscribeData))
+#define ALSASEQ_IS_SUBSCRIBE_DATA(obj)                          \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                          \
+                                ALSASEQ_TYPE_SUBSCRIBE_DATA))
+
+#define ALSASEQ_SUBSCRIBE_DATA_CLASS(klass)                     \
+    (G_TYPE_CHECK_CLASS_CAST((klass),                           \
+                             ALSASEQ_TYPE_SUBSCRIBE_DATA,       \
+                             ALSASeqSubscribeDataClass))
+#define ALSASEQ_IS_SUBSCRIBE_DATA_CLASS(klass)                  \
+    (G_TYPE_CHECK_CLASS_TYPE((klass),                           \
+                             ALSASEQ_TYPE_SUBSCRIBE_DATA))
+#define ALSASEQ_SUBSCRIBE_DATA_GET_CLASS(obj)                   \
+    (G_TYPE_INSTANCE_GET_CLASS((obj),                           \
+                               ALSASEQ_TYPE_SUBSCRIBE_DATA,     \
+                               ALSASeqSubscribeDataClass))
+
+typedef struct _ALSASeqSubscribeData            ALSASeqSubscribeData;
+typedef struct _ALSASeqSubscribeDataClass       ALSASeqSubscribeDataClass;
+
+struct _ALSASeqSubscribeData {
+    GObject parent_instance;
+};
+
+struct _ALSASeqSubscribeDataClass {
+    GObjectClass parent_class;
+};
+
+GType alsaseq_subscribe_data_get_type() G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif
