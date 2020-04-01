7c36f1cfde65098bccd4a0f6af46eee37e6d3bcc
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-01 18:13:28 +0900

    seq: event: add GObject abstract object to represent event in ALSA Sequencer
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ src/seq/event.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "event.h"
+
+G_DEFINE_ABSTRACT_TYPE(ALSASeqEvent, alsaseq_event, G_TYPE_OBJECT)
+
+static void alsaseq_event_class_init(ALSASeqEventClass *klass)
+{
+    return;
+}
+
+static void alsaseq_event_init(ALSASeqEvent *self)
+{
+    return;
+}
