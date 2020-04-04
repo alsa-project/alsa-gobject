alsa-project/alsa-gobject

commit 4277625a6bb1b7852ffb1a4a089b972c72f264b4

ctl remove 'transfer full' annoation from constructors
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/ctl/elem-id.c
@@ -14,7 +14,7 @@ G_DEFINE_BOXED_TYPE(ALSACtlElemId, alsactl_elem_id, ctl_elem_id_copy, g_free);
  *
  * Allocates and return an instance of ALSACtlElemId by the numerical ID.
  *
- * Returns: (transfer full): A #ALSACtlElemId.
+ * Returns: A #ALSACtlElemId.
  */
 ALSACtlElemId *alsactl_elem_id_new_by_numid(guint numid)
 {

src/ctl/elem-info-bool.c
@@ -75,7 +75,7 @@ static void alsactl_elem_info_bool_init(ALSACtlElemInfoBool *self)
  *
  * Allocate and return an instance of ALSACtlElemInfoBool.
  *
- * Returns: (transfer full): A #ALSACtlElemInfoBool.
+ * Returns: A #ALSACtlElemInfoBool.
  */
 ALSACtlElemInfoBool *alsactl_elem_info_bool_new()
 {

src/ctl/elem-info-bytes.c
@@ -75,7 +75,7 @@ static void alsactl_elem_info_bytes_init(ALSACtlElemInfoBytes *self)
  *
  * Allocate and return an instance of ALSACtlElemInfoBytes.
  *
- * Returns: (transfer full): A #ALSACtlElemInfoBytes.
+ * Returns: A #ALSACtlElemInfoBytes.
  */
 ALSACtlElemInfoBytes *alsactl_elem_info_bytes_new()
 {

src/ctl/elem-info-enum.c
@@ -110,7 +110,7 @@ static void alsactl_elem_info_enum_init(ALSACtlElemInfoEnum *self)
  *
  * Allocate and return an instance of ALSACtlElemInfoEnum.
  *
- * Returns: (transfer full): A #ALSACtlElemInfoEnum.
+ * Returns: A #ALSACtlElemInfoEnum.
  */
 ALSACtlElemInfoEnum *alsactl_elem_info_enum_new()
 {

src/ctl/elem-info-iec60958.c
@@ -18,7 +18,7 @@ static void alsactl_elem_info_iec60958_init(ALSACtlElemInfoIec60958 *self)
  *
  * Allocate and return an instance of ALSACtlElemInfoIec60958.
  *
- * Returns: (transfer full): A #ALSACtlElemInfoIec60958.
+ * Returns: A #ALSACtlElemInfoIec60958.
  */
 ALSACtlElemInfoIec60958 *alsactl_elem_info_iec60958_new()
 {

src/ctl/elem-info-int.c
@@ -116,7 +116,7 @@ static void alsactl_elem_info_int_init(ALSACtlElemInfoInt *self)
  *
  * Allocate and return an instance of ALSACtlElemInfoInt.
  *
- * Returns: (transfer full): A #ALSACtlElemInfoInt.
+ * Returns: A #ALSACtlElemInfoInt.
  */
 ALSACtlElemInfoInt *alsactl_elem_info_int_new()
 {

commit 82c1bccc9d44adcfb9c1a9aa1c9eebfc606419df

ctl: ensure to remove 'transfer full' annoation from constructors
Fixes: 4277625a6bb1: ("ctl remove 'transfer full' annoation from constructors")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/ctl/elem-id.c
@@ -37,7 +37,7 @@ ALSACtlElemId *alsactl_elem_id_new_by_numid(guint numid)
  *
  * Allocates and return an instance of ALSACtlElemId by the name.
  *
- * Returns: (transfer full): A #ALSACtlElemId.
+ * Returns: A #ALSACtlElemId.
  */
 ALSACtlElemId *alsactl_elem_id_new_by_name(ALSACtlElemIfaceType iface,
                                            guint device_id, guint subdevice_id,

src/ctl/elem-info-int64.c
@@ -116,7 +116,7 @@ static void alsactl_elem_info_int64_init(ALSACtlElemInfoInt64 *self)
  *
  * Allocate and return an instance of ALSACtlElemInfoInt64.
  *
- * Returns: (transfer full): A #ALSACtlElemInfoInt64.
+ * Returns: A #ALSACtlElemInfoInt64.
  */
 ALSACtlElemInfoInt64 *alsactl_elem_info_int64_new()
 {

src/ctl/elem-value.c
@@ -57,7 +57,7 @@ static void alsactl_elem_value_init(ALSACtlElemValue *self)
  *
  * Allocate and return an instance of ALSACtlElemValue.
  *
- * Returns: (transfer full): A #ALSACtlElemValue.
+ * Returns: A #ALSACtlElemValue.
  */
 ALSACtlElemValue *alsactl_elem_value_new()
 {