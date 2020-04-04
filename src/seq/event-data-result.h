0f333f1fd1b46f38ba214ce0cf7f8a7f4ea94f21
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-04-04 13:51:26 +0900

    seq: event_data_result: change function signature to return nothing
    The alsa-gobject project has a loose convention to have functions
    returning nothing.
    
    Fixes: c8003a103393 ("seq: event_data_result: add accessor methods")
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/event-data-result.h
+++ src/seq/event-data-result.h
@@ -17,13 +17,13 @@ typedef struct snd_seq_result ALSASeqEventDataResult;
 
 GType alsaseq_event_data_result_get_type() G_GNUC_CONST;
 
-ALSASeqEventType alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self);
-
+void alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self,
+                                         ALSASeqEventType *event_type);
 void alsaseq_event_data_result_set_event(ALSASeqEventDataResult *self,
                                          ALSASeqEventType event_type);
 
-gint alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self);
-
+void alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self,
+                                          gint *result);
 void alsaseq_event_data_result_set_result(ALSASeqEventDataResult *self,
                                           gint result);
 

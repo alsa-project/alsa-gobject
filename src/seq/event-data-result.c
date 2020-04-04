alsa-project/alsa-gobject

commit c8003a1033931dc215e3e2f8bc476ff6ec6ed65a

seq: event_data_result: add accessor methods
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-data-result.c
@@ -7,3 +7,55 @@ ALSASeqEventDataResult *seq_event_data_result_copy(const ALSASeqEventDataResult 
 }
 
 G_DEFINE_BOXED_TYPE(ALSASeqEventDataResult, alsaseq_event_data_result, seq_event_data_result_copy, g_free)
+
+/**
+ * alsaseq_event_data_result_get_event:
+ * @self: A #ALSASeqEventDataResult.
+ *
+ * Get the type of event in which the data results.
+ *
+ * Returns: The type of event in which the data results.
+ */
+ALSASeqEventType alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self)
+{
+    return (ALSASeqEventType)self->event;
+}
+
+/**
+ * alsaseq_event_data_result_set_event:
+ * @self: A #ALSASeqEventDataResult.
+ * @event_type: A #ALSASeqEventType.
+ *
+ * Set the type of event in which the data results.
+ */
+void alsaseq_event_data_result_set_event(ALSASeqEventDataResult *self,
+                                         ALSASeqEventType event_type)
+{
+    self->event = (int)event_type;
+}
+
+/**
+ * alsaseq_event_data_result_get_result:
+ * @self: A #ALSASeqEventDataResult.
+ *
+ * Get the status of event.
+ *
+ * Returns: the status of the event.
+ */
+gint alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self)
+{
+    return self->result;
+}
+
+/**
+ * alsaseq_event_data_result_set_result:
+ * @self: A #ALSASeqEventDataResult.
+ * @result: The status of event.
+ *
+ * Set the status of event.
+ */
+void alsaseq_event_data_result_set_result(ALSASeqEventDataResult *self,
+                                          gint result)
+{
+    self->result = result;
+}

src/seq/event-data-result.h
@@ -5,6 +5,8 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include <seq/alsaseq-enums.h>
+
 #include <sound/asequencer.h>
 
 G_BEGIN_DECLS
@@ -14,6 +16,16 @@ G_BEGIN_DECLS
 typedef struct snd_seq_result ALSASeqEventDataResult;
 
 GType alsaseq_event_data_result_get_type() G_GNUC_CONST;
+
+ALSASeqEventType alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self);
+
+void alsaseq_event_data_result_set_event(ALSASeqEventDataResult *self,
+                                         ALSASeqEventType event_type);
+
+gint alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self);
+
+void alsaseq_event_data_result_set_result(ALSASeqEventDataResult *self,
+                                          gint result);
 
 G_END_DECLS
 

src/seq/alsaseq.map
@@ -72,6 +72,10 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_event_variable_set_data";
 
     "alsaseq_event_data_result_get_type";
+    "alsaseq_event_data_result_get_event";
+    "alsaseq_event_data_result_set_event";
+    "alsaseq_event_data_result_get_result";
+    "alsaseq_event_data_result_set_result";
   local:
     *;
 };

commit 0f333f1fd1b46f38ba214ce0cf7f8a7f4ea94f21

seq: event_data_result: change function signature to return nothing
The alsa-gobject project has a loose convention to have functions
returning nothing.

Fixes: c8003a103393 ("seq: event_data_result: add accessor methods")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-data-result.c
@@ -11,14 +11,14 @@ G_DEFINE_BOXED_TYPE(ALSASeqEventDataResult, alsaseq_event_data_result, seq_event
 /**
  * alsaseq_event_data_result_get_event:
  * @self: A #ALSASeqEventDataResult.
+ * @event_type: (out): The type of event in which the data results.
  *
  * Get the type of event in which the data results.
- *
- * Returns: The type of event in which the data results.
  */
-ALSASeqEventType alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self)
+void alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self,
+                                         ALSASeqEventType *event_type)
 {
-    return (ALSASeqEventType)self->event;
+    *event_type = (ALSASeqEventType)self->event;
 }
 
 /**
@@ -37,14 +37,14 @@ void alsaseq_event_data_result_set_event(ALSASeqEventDataResult *self,
 /**
  * alsaseq_event_data_result_get_result:
  * @self: A #ALSASeqEventDataResult.
+ * @result: (out): the status of the event.
  *
  * Get the status of event.
- *
- * Returns: the status of the event.
  */
-gint alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self)
+void alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self,
+                                          gint *result)
 {
-    return self->result;
+    *result = self->result;
 }
 
 /**

src/seq/event-data-result.h
@@ -17,13 +17,13 @@ typedef struct snd_seq_result ALSASeqEventDataResult;
 
 GType alsaseq_event_data_result_get_type() G_GNUC_CONST;
 
-ALSASeqEventType alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self);
-
+void alsaseq_event_data_result_get_event(ALSASeqEventDataResult *self,
+                                         ALSASeqEventType *event_type);
 void alsaseq_event_data_result_set_event(ALSASeqEventDataResult *self,
                                          ALSASeqEventType event_type);
 
-gint alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self);
-
+void alsaseq_event_data_result_get_result(ALSASeqEventDataResult *self,
+                                          gint *result);
 void alsaseq_event_data_result_set_result(ALSASeqEventDataResult *self,
                                           gint result);
 