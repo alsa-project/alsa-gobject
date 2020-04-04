alsa-project/alsa-gobject

commit 82f76999235243a9b0006893d808cacb570c74ce

seq: event_data_connect: add accessor methods
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-data-connect.c
@@ -7,3 +7,55 @@ ALSASeqEventDataConnect *seq_event_data_connect_copy(const ALSASeqEventDataConne
 }
 
 G_DEFINE_BOXED_TYPE(ALSASeqEventDataConnect, alsaseq_event_data_connect, seq_event_data_connect_copy, g_free)
+
+/**
+ * alsaseq_event_data_connect_get_src:
+ * @self: A #ALSASeqEventDataConnect.
+ *
+ * Get the source of connection event.
+ *
+ * Returns: (transfer none): The source of connection event.
+ */
+const ALSASeqAddr *alsaseq_event_data_connect_get_src(ALSASeqEventDataConnect *self)
+{
+    return &self->sender;
+}
+
+/**
+ * alsaseq_event_data_connect_set_src:
+ * @self: A #ALSASeqEventDataConnect.
+ * @src: A #ALSASeqAddr.
+ *
+ * Set the source to the connection event.
+ */
+void alsaseq_event_data_connect_set_src(ALSASeqEventDataConnect *self,
+                                        const ALSASeqAddr *src)
+{
+    self->sender = *src;
+}
+
+/**
+ * alsaseq_event_data_connect_get_dst:
+ * @self: A #ALSASeqEventDataConnect.
+ *
+ * Get the source of connection event.
+ *
+ * Returns: (transfer none): The source of connection event.
+ */
+const ALSASeqAddr *alsaseq_event_data_connect_get_dst(ALSASeqEventDataConnect *self)
+{
+    return &self->dest;
+}
+
+/**
+ * alsaseq_event_data_connect_set_dst:
+ * @self: A #ALSASeqEventDataConnect.
+ * @dst: A #ALSASeqAddr.
+ *
+ * Set the source to the connection event.
+ */
+void alsaseq_event_data_connect_set_dst(ALSASeqEventDataConnect *self,
+                                        const ALSASeqAddr *dst)
+{
+    self->dest = *dst;
+}

src/seq/event-data-connect.h
@@ -5,6 +5,8 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include <seq/addr.h>
+
 #include <sound/asequencer.h>
 
 G_BEGIN_DECLS
@@ -14,6 +16,14 @@ G_BEGIN_DECLS
 typedef struct snd_seq_connect ALSASeqEventDataConnect;
 
 GType alsaseq_event_data_connect_get_type() G_GNUC_CONST;
+
+const ALSASeqAddr *alsaseq_event_data_connect_get_src(ALSASeqEventDataConnect *self);
+void alsaseq_event_data_connect_set_src(ALSASeqEventDataConnect *self,
+                                        const ALSASeqAddr *src);
+
+const ALSASeqAddr *alsaseq_event_data_connect_get_dst(ALSASeqEventDataConnect *self);
+void alsaseq_event_data_connect_set_dst(ALSASeqEventDataConnect *self,
+                                        const ALSASeqAddr *dst);
 
 G_END_DECLS
 

src/seq/alsaseq.map
@@ -112,6 +112,10 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_event_data_queue_set_quadlet_param";
 
     "alsaseq_event_data_connect_get_type";
+    "alsaseq_event_data_connect_get_src";
+    "alsaseq_event_data_connect_set_src";
+    "alsaseq_event_data_connect_get_dst";
+    "alsaseq_event_data_connect_set_dst";
   local:
     *;
 };

commit e38de3d59a87c4d3c934f05321d124848c5bd647

seq: event_data_connect: change function signature to return nothing
The alsa-gobject project has a loose convention to have functions
returning nothing.

Fixes: 82f769992352: ("seq: event_data_connect: add accessor methods")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-data-connect.c
@@ -11,14 +11,14 @@ G_DEFINE_BOXED_TYPE(ALSASeqEventDataConnect, alsaseq_event_data_connect, seq_eve
 /**
  * alsaseq_event_data_connect_get_src:
  * @self: A #ALSASeqEventDataConnect.
+ * @src: (out)(transfer none): The source of connection event.
  *
  * Get the source of connection event.
- *
- * Returns: (transfer none): The source of connection event.
  */
-const ALSASeqAddr *alsaseq_event_data_connect_get_src(ALSASeqEventDataConnect *self)
+void alsaseq_event_data_connect_get_src(ALSASeqEventDataConnect *self,
+                                        const ALSASeqAddr **src)
 {
-    return &self->sender;
+    *src = &self->sender;
 }
 
 /**
@@ -37,14 +37,14 @@ void alsaseq_event_data_connect_set_src(ALSASeqEventDataConnect *self,
 /**
  * alsaseq_event_data_connect_get_dst:
  * @self: A #ALSASeqEventDataConnect.
+ * @dst: (out)(transfer none): The destination of connection event.
  *
- * Get the source of connection event.
- *
- * Returns: (transfer none): The source of connection event.
+ * Get the destination of connection event.
  */
-const ALSASeqAddr *alsaseq_event_data_connect_get_dst(ALSASeqEventDataConnect *self)
+void alsaseq_event_data_connect_get_dst(ALSASeqEventDataConnect *self,
+                                        const ALSASeqAddr **dst)
 {
-    return &self->dest;
+    *dst = &self->dest;
 }
 
 /**

src/seq/event-data-connect.h
@@ -17,11 +17,13 @@ typedef struct snd_seq_connect ALSASeqEventDataConnect;
 
 GType alsaseq_event_data_connect_get_type() G_GNUC_CONST;
 
-const ALSASeqAddr *alsaseq_event_data_connect_get_src(ALSASeqEventDataConnect *self);
+void alsaseq_event_data_connect_get_src(ALSASeqEventDataConnect *self,
+                                        const ALSASeqAddr **src);
 void alsaseq_event_data_connect_set_src(ALSASeqEventDataConnect *self,
                                         const ALSASeqAddr *src);
 
-const ALSASeqAddr *alsaseq_event_data_connect_get_dst(ALSASeqEventDataConnect *self);
+void alsaseq_event_data_connect_get_dst(ALSASeqEventDataConnect *self,
+                                        const ALSASeqAddr **dst);
 void alsaseq_event_data_connect_set_dst(ALSASeqEventDataConnect *self,
                                         const ALSASeqAddr *dst);
 