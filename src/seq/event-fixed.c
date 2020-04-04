alsa-project/alsa-gobject

commit 641f317f04b3caeeae696c62beb273a477fdac2d

seq: event_fixed: add API to refer to byte data of event
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-fixed.c
@@ -194,3 +194,44 @@ ALSASeqEventFixed *alsaseq_event_fixed_new(ALSASeqEventType event_type,
                         "length-mode", ALSASEQ_EVENT_LENGTH_MODE_FIXED,
                         NULL);
 }
+
+/**
+ * alsaseq_event_fixed_get_byte_data:
+ * @self: A #ALSASeqEventFixed.
+ *
+ * Refer to the 12 byte data for the event.
+ *
+ * Returns: (array fixed-size=12)(transfer none): The 12 byte data for the
+ *          event. The lifetime of the object is the same as the event itself.
+ */
+const guint8 *alsaseq_event_fixed_get_byte_data(ALSASeqEventFixed *self)
+{
+    ALSASeqEvent *parent;
+    struct snd_seq_event *ev;
+
+    g_return_val_if_fail(ALSASEQ_IS_EVENT_FIXED(self), NULL);
+    parent = ALSASEQ_EVENT(self);
+    seq_event_refer_private(parent, &ev);
+
+    return ev->data.raw8.d;
+}
+
+/**
+ * alsaseq_event_fixed_set_byte_data:
+ * @self: A #ALSASeqEventFixed.
+ * @data: (array fixed-size=12)(transfer none): The 12 byte data for the event.
+ *
+ * Copy the 12 byte data for the event.
+ */
+void alsaseq_event_fixed_set_byte_data(ALSASeqEventFixed *self,
+                                       const guint8 data[12])
+{
+    ALSASeqEvent *parent;
+    struct snd_seq_event *ev;
+
+    g_return_if_fail(ALSASEQ_IS_EVENT_FIXED(self));
+    parent = ALSASEQ_EVENT(self);
+    seq_event_refer_private(parent, &ev);
+
+    memcpy(ev->data.raw8.d, data, sizeof(ev->data.raw8.d));
+}

src/seq/event-fixed.h
@@ -52,6 +52,10 @@ GType alsaseq_event_fixed_get_type() G_GNUC_CONST;
 ALSASeqEventFixed *alsaseq_event_fixed_new(ALSASeqEventType event_type,
                                            GError **error);
 
+const guint8 *alsaseq_event_fixed_get_byte_data(ALSASeqEventFixed *self);
+void alsaseq_event_fixed_set_byte_data(ALSASeqEventFixed *self,
+                                       const guint8 data[12]);
+
 G_END_DECLS
 
 #endif

src/seq/alsaseq.map
@@ -65,6 +65,8 @@ ALSA_GOBJECT_0_0_0 {
 
     "alsaseq_event_fixed_get_type";
     "alsaseq_event_fixed_new";
+    "alsaseq_event_fixed_get_byte_data";
+    "alsaseq_event_fixed_set_byte_data";
 
     "alsaseq_event_variable_get_type";
     "alsaseq_event_variable_new";

tests/alsaseq-event-fixed
@@ -20,6 +20,8 @@ props = (
 )
 methods = (
     'new',
+    'get_byte_data',
+    'set_byte_data',
 )
 signals = ()
 

commit 3242df11c7778638a6504755fbedfbf9ae990770

seq: event_fixed: change parameter names for byte and quadlet data
The getter/setter for byte and quadlet data has argument named as 'data'.
For this case, the alsa-gobject project has a loose convention to name them
as 'bytes' and 'quadlets'. This commit renames the arguments.

Fixes: 641f317f04b3 ("seq: event_fixed: add API to refer to byte data of event")
Fixes: 1b71f2015d06: ("seq: event_fixed: add API to refer to quadlet data of event")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-fixed.c
@@ -237,12 +237,12 @@ const guint8 *alsaseq_event_fixed_get_byte_data(ALSASeqEventFixed *self)
 /**
  * alsaseq_event_fixed_set_byte_data:
  * @self: A #ALSASeqEventFixed.
- * @data: (array fixed-size=12)(transfer none): The 12 byte data for the event.
+ * @bytes: (array fixed-size=12)(transfer none): The 12 byte data for the event.
  *
  * Copy the 12 byte data for the event.
  */
 void alsaseq_event_fixed_set_byte_data(ALSASeqEventFixed *self,
-                                       const guint8 data[12])
+                                       const guint8 bytes[12])
 {
     ALSASeqEvent *parent;
     struct snd_seq_event *ev;
@@ -251,7 +251,7 @@ void alsaseq_event_fixed_set_byte_data(ALSASeqEventFixed *self,
     parent = ALSASEQ_EVENT(self);
     seq_event_refer_private(parent, &ev);
 
-    memcpy(ev->data.raw8.d, data, sizeof(ev->data.raw8.d));
+    memcpy(ev->data.raw8.d, bytes, sizeof(ev->data.raw8.d));
 }
 
 /**
@@ -278,12 +278,13 @@ const guint32 *alsaseq_event_fixed_get_quadlet_data(ALSASeqEventFixed *self)
 /**
  * alsaseq_event_fixed_set_quadlet_data:
  * @self: A #ALSASeqEventFixed.
- * @data: (array fixed-size=3)(transfer none): The 3 quadlet data for the event.
+ * @quadlets: (array fixed-size=3)(transfer none): The 3 quadlet data for the
+ *            event.
  *
  * Copy the 3 quadlet data for the event.
  */
 void alsaseq_event_fixed_set_quadlet_data(ALSASeqEventFixed *self,
-                                          const guint32 data[3])
+                                          const guint32 quadlets[3])
 {
     ALSASeqEvent *parent;
     struct snd_seq_event *ev;
@@ -292,5 +293,5 @@ void alsaseq_event_fixed_set_quadlet_data(ALSASeqEventFixed *self,
     parent = ALSASEQ_EVENT(self);
     seq_event_refer_private(parent, &ev);
 
-    memcpy(ev->data.raw32.d, data, sizeof(ev->data.raw32.d));
+    memcpy(ev->data.raw32.d, quadlets, sizeof(ev->data.raw32.d));
 }

src/seq/event-fixed.h
@@ -54,11 +54,11 @@ ALSASeqEventFixed *alsaseq_event_fixed_new(ALSASeqEventType event_type,
 
 const guint8 *alsaseq_event_fixed_get_byte_data(ALSASeqEventFixed *self);
 void alsaseq_event_fixed_set_byte_data(ALSASeqEventFixed *self,
-                                       const guint8 data[12]);
+                                       const guint8 bytes[12]);
 
 const guint32 *alsaseq_event_fixed_get_quadlet_data(ALSASeqEventFixed *self);
 void alsaseq_event_fixed_set_quadlet_data(ALSASeqEventFixed *self,
-                                          const guint32 data[3]);
+                                          const guint32 quadlets[3]);
 
 G_END_DECLS
 