alsa-project/alsa-gobject

commit 9d535a87381c83af7a8c48154afaaada8606bc97

seq: user_client: add an API to schedule event
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -312,3 +312,62 @@ void alsaseq_user_client_get_pool(ALSASeqUserClient *self,
     if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_GET_CLIENT_POOL, pool) < 0)
         generate_error(error, errno);
 }
+
+/**
+ * alsaseq_user_client_schedule_event:
+ * @self: A #ALSASeqUserClient.
+ * @event: An instance of #ALSASeqEvent to schedule.
+ * @error: A #GError.
+ *
+ * Deliver the event immediately, or schedule it into memory pool of the client.
+ */
+void alsaseq_user_client_schedule_event(ALSASeqUserClient *self,
+                                        ALSASeqEvent *event, GError **error)
+{
+    ALSASeqUserClientPrivate *priv;
+    struct snd_seq_event *ev;
+    ssize_t len;
+    guint8 *ptr;
+
+    g_return_if_fail(ALSASEQ_IS_USER_CLIENT(self));
+    g_return_if_fail(ALSASEQ_IS_EVENT(event));
+    priv = alsaseq_user_client_get_instance_private(self);
+    seq_event_refer_private(event, &ev);
+
+    switch (ev->flags & SNDRV_SEQ_EVENT_LENGTH_MASK) {
+    case SNDRV_SEQ_EVENT_LENGTH_FIXED:
+        ptr = NULL;
+        len = sizeof(*ev);
+        break;
+    case SNDRV_SEQ_EVENT_LENGTH_VARIABLE:
+        if (ev->data.ext.len == 0 || ev->data.ext.ptr == NULL) {
+            generate_error(error, EINVAL);
+            return;
+        }
+
+        len = sizeof(*ev) + ev->data.ext.len;
+        ptr = g_malloc0(len);
+        if (ptr == NULL) {
+            generate_error(error, ENOMEM);
+            return;
+        }
+
+        memcpy(ptr, ev, sizeof(*ev));
+        memcpy(ptr + sizeof(*ev), ev->data.ext.ptr, ev->data.ext.len);
+
+        ev = (struct snd_seq_event *)ptr;
+        break;
+    case SNDRV_SEQ_EVENT_LENGTH_VARUSR:
+        // Unsupported since it handles raw pointer which is difficult to
+        // be exposed by interfaces capable for g-i.
+    default:
+        break;
+    }
+
+    len = write(priv->fd, ev, len);
+    if (len < 0)
+        generate_error(error, errno);
+
+    if (ptr != NULL)
+        g_free(ptr);
+}

src/seq/user-client.h
@@ -8,6 +8,7 @@
 #include <seq/client-info.h>
 #include <seq/port-info.h>
 #include <seq/client-pool.h>
+#include <seq/event.h>
 
 G_BEGIN_DECLS
 
@@ -80,6 +81,9 @@ void alsaseq_user_client_set_pool(ALSASeqUserClient *self,
 void alsaseq_user_client_get_pool(ALSASeqUserClient *self,
                                   ALSASeqClientPool *const *client_pool,
                                   GError **error);
+
+void alsaseq_user_client_schedule_event(ALSASeqUserClient *self,
+                                        ALSASeqEvent *event, GError **error);
 
 G_END_DECLS
 

src/seq/alsaseq.map
@@ -42,6 +42,7 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_user_client_delete_port";
     "alsaseq_user_client_set_pool";
     "alsaseq_user_client_get_pool";
+    "alsaseq_user_client_schedule_event";
 
     "alsaseq_addr_get_type";
     "alsaseq_addr_new";

tests/alsaseq-user-client
@@ -23,6 +23,7 @@ methods = (
     'delete_port',
     'set_pool',
     'get_pool',
+    'schedule_event',
 )
 signals = ()
 

commit 59d838b6de9bccb7a778190595b6d3cb735cac31

seq: user_client: move header includsion for subclass of ALSASeqEvent
Fixes: 9d535a87381c: ("seq: user_client: add an API to schedule event")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "user-client.h"
 #include "query.h"
-#include "event-fixed.h"
-#include "event-variable.h"
 #include "privates.h"
 
 #include <sys/types.h>

src/seq/user-client.h
@@ -9,6 +9,8 @@
 #include <seq/port-info.h>
 #include <seq/client-pool.h>
 #include <seq/event.h>
+#include <seq/event-fixed.h>
+#include <seq/event-variable.h>
 #include <seq/subscribe-data.h>
 
 G_BEGIN_DECLS