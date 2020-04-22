alsa-project/alsa-gobject

commit 89b631bd3e2541136ce88d77d40e003110ef2a86

seq: user_client: add GObject signal to handle event
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -30,6 +30,12 @@ enum seq_user_client_prop_type {
 };
 static GParamSpec *seq_user_client_props[SEQ_USER_CLIENT_PROP_COUNT] = { NULL, };
 
+enum seq_user_client_sig_type {
+    SEQ_USER_CLIENT_SIG_TYPE_HANDLE_EVENT = 0,
+    SEQ_USER_CLIENT_SIG_TYPE_COUNT,
+};
+static guint seq_user_client_sigs[SEQ_USER_CLIENT_SIG_TYPE_COUNT] = { 0 };
+
 static void seq_user_client_get_property(GObject *obj, guint id, GValue *val,
                                          GParamSpec *spec)
 {
@@ -76,6 +82,22 @@ static void alsaseq_user_client_class_init(ALSASeqUserClientClass *klass)
     g_object_class_install_properties(gobject_class,
                                       SEQ_USER_CLIENT_PROP_COUNT,
                                       seq_user_client_props);
+
+    /**
+     * ALSASeqUserClient::handle-event:
+     * @self: A #ALSASeqUserClient.
+     * @event: (transfer none): A #ALSASeqEvent or derived objects.
+     *
+     * When event occurs, this signal is emit with an object for the event.
+     */
+    seq_user_client_sigs[SEQ_USER_CLIENT_SIG_TYPE_HANDLE_EVENT] =
+        g_signal_new("handle-event",
+                     G_OBJECT_CLASS_TYPE(klass),
+                     G_SIGNAL_RUN_LAST,
+                     0,
+                     NULL, NULL,
+                     g_cclosure_marshal_VOID__OBJECT,
+                     G_TYPE_NONE, 1, ALSASEQ_TYPE_EVENT);
 }
 
 static void alsaseq_user_client_init(ALSASeqUserClient *self)

commit d3c52aeafb15db4250d6186dc3cd3c6e3aad87a1

seq: user_client: fix annotation for GObject signal handler
Any object from ALSASeqEvent is passed to the second argument for handler
of 'handle-event' GObject signal, and it's not ALSASeqEvent itself.

Fixes: 89b631bd3e25 ("seq: user_client: add GObject signal to handle event")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -88,7 +88,7 @@ static void alsaseq_user_client_class_init(ALSASeqUserClientClass *klass)
     /**
      * ALSASeqUserClient::handle-event:
      * @self: A #ALSASeqUserClient.
-     * @event: (transfer none): A #ALSASeqEvent or derived objects.
+     * @event: (transfer none): An object derived from #ALSASeqEvent.
      *
      * When event occurs, this signal is emit with an object for the event.
      */

src/seq/user-client.h
@@ -58,7 +58,7 @@ struct _ALSASeqUserClientClass {
     /**
      * ALSASeqUserClientClass::handle_event:
      * @self: A #ALSASeqUserClient.
-     * @event: (transfer none): A #ALSASeqEvent or derived objects.
+     * @event: (transfer none): An object derived from #ALSASeqEvent.
      *
      * When event occurs, this signal is emit with an object for the event.
      */