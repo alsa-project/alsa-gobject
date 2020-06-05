bd2a4fbdc3b4b0c537f7b5f0818ab023d75b4b00
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-06-05 23:41:57 +0900

    hwdep: fulfill documentation for ALSAHwdep
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/hwdep/query.c
+++ src/hwdep/query.c
@@ -12,6 +12,13 @@
 
 #include <libudev.h>
 
+/**
+ * SECTION: query
+ * @Title: Global functions in ALSAHwdep
+ * @Short_description: Global functions available without holding any file
+ *                     descriptor
+ */
+
 // For error handling.
 G_DEFINE_QUARK("alsahwdep-error", alsahwdep_error)
 
