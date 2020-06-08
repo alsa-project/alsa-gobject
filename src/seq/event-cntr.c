721b780cd4c818b5a828d35af84467dbf32b7521
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-06-08 23:10:10 +0900

    seq: event_cntr: add accesor APIs for result data of event pointed by index
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/event-cntr.c
+++ src/seq/event-cntr.c
@@ -1444,3 +1444,65 @@ void alsaseq_event_cntr_set_connect_data(ALSASeqEventCntr *self, gsize index,
 
     ev->data.connect = *(struct snd_seq_connect *)data;
 }
+
+/**
+ * alsaseq_event_cntr_get_result_data:
+ * @self: A #ALSASeqEventCntr.
+ * @index: The index of event to set.
+ * @data: (out)(transfer none): The result data of event.
+ * @error: A #GError.
+ *
+ * Get the result data of event pointed by the index.
+ */
+void alsaseq_event_cntr_get_result_data(ALSASeqEventCntr *self, gsize index,
+                            const ALSASeqEventDataResult **data, GError **error)
+{
+    ALSASeqEventCntrPrivate *priv;
+    struct event_iterator iter;
+    struct snd_seq_event *ev;
+
+    g_return_if_fail(ALSASEQ_IS_EVENT_CNTR(self));
+    priv = alsaseq_event_cntr_get_instance_private(self);
+
+    event_iterator_init(&iter, priv->buf, priv->length, priv->allocated);
+    ev = event_iterator_find(&iter, index);
+    if (ev == NULL) {
+        generate_error(error, ENOENT);
+        return;
+    }
+
+    *data = (const ALSASeqEventDataResult *)&ev->data.result;
+}
+
+/**
+ * alsaseq_event_cntr_set_result_data:
+ * @self: A #ALSASeqEventCntr.
+ * @index: The index of event to set.
+ * @data: The result data of event.
+ * @error: A #GError.
+ *
+ * Copy the result data to the event pointed by the index.
+ */
+void alsaseq_event_cntr_set_result_data(ALSASeqEventCntr *self, gsize index,
+                            const ALSASeqEventDataResult *data, GError **error)
+{
+    ALSASeqEventCntrPrivate *priv;
+    struct event_iterator iter;
+    struct snd_seq_event *ev;
+
+    g_return_if_fail(ALSASEQ_IS_EVENT_CNTR(self));
+    priv = alsaseq_event_cntr_get_instance_private(self);
+
+    event_iterator_init(&iter, priv->buf, priv->length, priv->allocated);
+    ev = event_iterator_find(&iter, index);
+    if (ev == NULL) {
+        generate_error(error, ENOENT);
+        return;
+    }
+
+    ensure_fixed_length_event(priv, ev, error);
+    if (*error != NULL)
+        return;
+
+    ev->data.result = *(struct snd_seq_result *)data;
+}
