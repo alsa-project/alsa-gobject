c6c6356a985da671da3ee29ff0df655c51d04389
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-06-08 23:10:10 +0900

    seq: event_fixed: dismiss ALSASeqEventFixed
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/event-data-note.c
+++ src/seq/event-data-note.c
@@ -7,7 +7,7 @@
  * @Short_description: A boxed object to represent data of note event
  *
  * A #ALSASeqEventDataNote is a boxed object to represent data of note event.
- * The instance of object is one of data properties in #ALSASeqEventFixed.
+ * The instance of object is one of data properties in event.
  *
  * The object wraps 'struct snd_seq_ev_note' in UAPI of Linux sound subsystem.
  */
--- src/seq/event-data-queue.c
+++ src/seq/event-data-queue.c
@@ -7,7 +7,7 @@
  * @Short_description: A boxed object to represent data of queue event
  *
  * A #ALSASeqEventDataQueue is a boxed object to represent data of queue event.
- * The instance of object is one of data properties in #ALSASeqEventFixed.
+ * The instance of object is one of data properties in event.
  *
  * The object wraps 'struct snd_seq_ev_queue_control' in UAPI of Linux sound
  * subsystem.
