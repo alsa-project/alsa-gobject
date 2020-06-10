e8dfdf8e429bf3cbb3747deee4621106a336231f
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-06-10 12:36:46 +0900

    timer: event_data_tstamp: add accessor APIs for tstamp event
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/timer/event-data-tstamp.c
+++ src/timer/event-data-tstamp.c
@@ -19,3 +19,44 @@ ALSATimerEventDataTstamp *timer_event_data_tstamp_copy(const ALSATimerEventDataT
 }
 
 G_DEFINE_BOXED_TYPE(ALSATimerEventDataTstamp, alsatimer_event_data_tstamp, timer_event_data_tstamp_copy, g_free)
+
+/**
+ * alsatimer_event_data_tstamp_get_event:
+ * @self: A #ALSATimerEventDataTstamp.
+ * @event: (out): The type of tstamp event, one of ALSATimerEventType.
+ *
+ * Get the kind of event for the timestamp event.
+ */
+void alsatimer_event_data_tstamp_get_event(ALSATimerEventDataTstamp *self,
+                                           ALSATimerEventType *event)
+{
+    *event = (ALSATimerEventType)self->event;
+}
+
+/**
+ * alsatimer_event_data_tstamp_get_tstamp:
+ * @self: A #ALSATimerEventDataTstamp.
+ * @tv_sec: (out): The seconds part of timestamp.
+ * @tv_nsec: (out): The nanoseconds part of timestamp.
+ *
+ * Get the seconds and nanoseconds part for the timestamp event.
+ */
+void alsatimer_event_data_tstamp_get_tstamp(ALSATimerEventDataTstamp *self,
+                                            guint *tv_sec, guint *tv_nsec)
+{
+    *tv_sec = (guint)self->tstamp.tv_sec;
+    *tv_nsec = (guint)self->tstamp.tv_nsec;
+}
+
+/**
+ * alsatimer_event_data_tstamp_get_val:
+ * @self: A #ALSATimerEventDataTstamp.
+ * @val: (out): The value depending on the type of timestamp event.
+ *
+ * Get the value depending on the type of timestamp event.
+ */
+void alsatimer_event_data_tstamp_get_val(ALSATimerEventDataTstamp *self,
+                                         guint *val)
+{
+    *val = self->val;
+}
