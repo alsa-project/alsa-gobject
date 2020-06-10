2e24b3e459b3180560f7bf95b5222a48fa97353d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-06-10 10:31:47 +0900

    timer: event_data_tick: become boxed object
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/timer/event-data-tick.h
+++ src/timer/event-data-tick.h
@@ -5,45 +5,13 @@
 #include <glib.h>
 #include <glib-object.h>
 
-#include <timer/event-data.h>
+#include <sound/asound.h>
 
 G_BEGIN_DECLS
 
 #define ALSATIMER_TYPE_EVENT_DATA_TICK   (alsatimer_event_data_tick_get_type())
 
-#define ALSATIMER_EVENT_DATA_TICK(obj)                              \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                              \
-                                ALSATIMER_TYPE_EVENT_DATA_TICK,     \
-                                ALSATimerEventDataTick))
-#define ALSATIMER_IS_EVENT_DATA_TICK(obj)                           \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                              \
-                                ALSATIMER_TYPE_EVENT_DATA_TICK))
-
-#define ALSATIMER_EVENT_DATA_TICK_CLASS(klass)                      \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                               \
-                             ALSATIMER_TYPE_EVENT_DATA_TICK,        \
-                             ALSATimerEventDataTickClass))
-#define ALSATIMER_IS_EVENT_DATA_TICK_CLASS(klass)                   \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                               \
-                             ALSATIMER_TYPE_EVENT_DATA_TICK))
-#define ALSATIMER_EVENT_DATA_TICK_GET_CLASS(obj)                    \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                               \
-                               ALSATIMER_TYPE_EVENT_DATA_TICK,      \
-                               ALSATimerEventDataTickkClass))
-
-typedef struct _ALSATimerEventDataTick          ALSATimerEventDataTick;
-typedef struct _ALSATimerEventDataTickClass     ALSATimerEventDataTickClass;
-typedef struct _ALSATimerEventDataTickPrivate   ALSATimerEventDataTickPrivate;
-
-struct _ALSATimerEventDataTick {
-    ALSATimerEventData parent_instance;
-
-    ALSATimerEventDataTickPrivate *priv;
-};
-
-struct _ALSATimerEventDataTickClass {
-    ALSATimerEventDataClass parent_class;
-};
+typedef struct snd_timer_read ALSATimerEventDataTick;
 
 GType alsatimer_event_data_tick_get_type() G_GNUC_CONST;
 
