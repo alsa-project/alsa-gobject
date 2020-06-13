alsa-project/alsa-gobject

commit 4198dc4f7854165e3764c962ca1b615e3cb42f9d

seq: user_client: add APIs to register usage of queue
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -661,3 +661,60 @@ void alsaseq_user_client_update_queue(ALSASeqUserClient *self,
     if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_CREATE_QUEUE, info) < 0)
         generate_error(error, errno);
 }
+
+/**
+ * alsaseq_user_client_get_queue_usage:
+ * @self: A #ALSASeqUserClient.
+ * @queue_id: The numerical ID of queue, except for entries in
+ *            ALSASeqSpecificQueueId.
+ * @use: (out): Whether the client uses the queue or not.
+ * @error: A #GError.
+ *
+ * Get usage of the queue by the client.
+ */
+void alsaseq_user_client_get_queue_usage(ALSASeqUserClient *self,
+                                         guint queue_id, gboolean *use,
+                                         GError **error)
+{
+    ALSASeqUserClientPrivate *priv;
+    struct snd_seq_queue_client data = {0};
+
+    g_return_if_fail(ALSASEQ_IS_USER_CLIENT(self));
+    priv = alsaseq_user_client_get_instance_private(self);
+
+    data.queue = (int)queue_id;
+    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_SET_QUEUE_CLIENT, &data) < 0) {
+        generate_error(error, errno);
+        return;
+    }
+
+    *use = data.used;
+}
+
+/**
+ * alsaseq_user_client_set_queue_usage:
+ * @self: A #ALSASeqUserClient.
+ * @queue_id: The numerical ID of queue, except for entries in
+ *            ALSASeqSpecificQueueId.
+ * @use: Whether to use the queue or not.
+ * @error: A #GError.
+ *
+ * Start the queue to use or not.
+ */
+void alsaseq_user_client_set_queue_usage(ALSASeqUserClient *self,
+                                         guint queue_id, gboolean use,
+                                         GError **error)
+{
+    ALSASeqUserClientPrivate *priv;
+    struct snd_seq_queue_client data = {0};
+
+    g_return_if_fail(ALSASEQ_IS_USER_CLIENT(self));
+    priv = alsaseq_user_client_get_instance_private(self);
+
+    data.queue = (int)queue_id;
+    data.client = priv->client_id;
+    data.used = use;
+
+    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_SET_QUEUE_CLIENT, &data) < 0)
+        generate_error(error, errno);
+}

src/seq/alsaseq.map
@@ -55,6 +55,8 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_user_client_create_queue";
     "alsaseq_user_client_delete_queue";
     "alsaseq_user_client_update_queue";
+    "alsaseq_user_client_get_queue_usage";
+    "alsaseq_user_client_set_queue_usage";
 
     "alsaseq_addr_get_type";
     "alsaseq_addr_new";

src/seq/user-client.h
@@ -113,6 +113,12 @@ void alsaseq_user_client_delete_queue(ALSASeqUserClient *self,
 void alsaseq_user_client_update_queue(ALSASeqUserClient *self,
                                 ALSASeqQueueInfo *queue_info, GError **error);
 
+void alsaseq_user_client_get_queue_usage(ALSASeqUserClient *self,
+                                         guint queue_id, gboolean *use,
+                                         GError **error);
+void alsaseq_user_client_set_queue_usage(ALSASeqUserClient *self,
+                                         guint queue_id, gboolean use,
+                                         GError **error);
 G_END_DECLS
 
 #endif

tests/alsaseq-user-client
@@ -29,6 +29,8 @@ methods = (
     'create_queue',
     'delete_queue',
     'update_queue',
+    'get_queue_usage',
+    'set_queue_usage',
 )
 signals = ()
 

commit 4db24f8ef3e3c1c112c0e376e8d6d7d1d311dcc4

seq: user_client: correct ioctl command to get the usage of queue by client
Fixes: 4198dc4f7854 ("seq: user_client: add APIs to register usage of queue")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -632,7 +632,7 @@ void alsaseq_user_client_get_queue_usage(ALSASeqUserClient *self,
     priv = alsaseq_user_client_get_instance_private(self);
 
     data.queue = (int)queue_id;
-    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_SET_QUEUE_CLIENT, &data) < 0) {
+    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_GET_QUEUE_CLIENT, &data) < 0) {
         generate_error(error, errno);
         return;
     }