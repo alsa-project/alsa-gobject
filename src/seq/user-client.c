alsa-project/alsa-gobject

commit 2ab3feab20d3951bf9f861380bbccf77717ed3d9

seq: user_client: add APIs to create/delete/update queue
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -588,3 +588,76 @@ void alsaseq_user_client_operate_subscription(ALSASeqUserClient *self,
     if (ioctl(priv->fd, request, data) < 0)
         generate_error(error, errno);
 }
+
+/**
+ * alsaseq_user_client_create_queue:
+ * @self: A #ALSASeqUserClient.
+ * @queue_info: The information of queue to add.
+ * @error: A #GError.
+ *
+ * Create a new queue owned by the client. The content of information is updated
+ * if success.
+ */
+void alsaseq_user_client_create_queue(ALSASeqUserClient *self,
+                                ALSASeqQueueInfo *queue_info, GError **error)
+{
+    ALSASeqUserClientPrivate *priv;
+    struct snd_seq_queue_info *info;
+
+    g_return_if_fail(ALSASEQ_IS_USER_CLIENT(self));
+    g_return_if_fail(ALSASEQ_IS_QUEUE_INFO(queue_info));
+    priv = alsaseq_user_client_get_instance_private(self);
+
+    seq_queue_info_refer_private(queue_info, &info);
+
+    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_CREATE_QUEUE, info) < 0)
+        generate_error(error, errno);
+}
+
+/**
+ * alsaseq_user_client_delete_queue:
+ * @self: A #ALSASeqUserClient.
+ * @queue_id: The numerical ID of queue, except for one of ALSASeqSpecificQueueId.
+ * @error: A #GError.
+ *
+ * Delete the queue owned by the client.
+ */
+void alsaseq_user_client_delete_queue(ALSASeqUserClient *self,
+                                      guint queue_id, GError **error)
+{
+    ALSASeqUserClientPrivate *priv;
+    struct snd_seq_queue_info info = {0};
+
+    g_return_if_fail(ALSASEQ_IS_USER_CLIENT(self));
+    priv = alsaseq_user_client_get_instance_private(self);
+
+    info.queue = (int)queue_id;
+    info.owner = priv->client_id;
+    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_DELETE_QUEUE, &info) < 0)
+        generate_error(error, errno);
+}
+
+/**
+ * alsaseq_user_client_update_queue:
+ * @self: A #ALSASeqUserClient.
+ * @queue_info: The information of queue to add.
+ * @error: A #GError.
+ *
+ * Update owned queue according to the information.
+ */
+void alsaseq_user_client_update_queue(ALSASeqUserClient *self,
+                                      ALSASeqQueueInfo *queue_info,
+                                      GError **error)
+{
+    ALSASeqUserClientPrivate *priv;
+    struct snd_seq_queue_info *info;
+
+    g_return_if_fail(ALSASEQ_IS_USER_CLIENT(self));
+    g_return_if_fail(ALSASEQ_IS_QUEUE_INFO(queue_info));
+    priv = alsaseq_user_client_get_instance_private(self);
+
+    seq_queue_info_refer_private(queue_info, &info);
+
+    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_CREATE_QUEUE, info) < 0)
+        generate_error(error, errno);
+}

src/seq/alsaseq.map
@@ -51,6 +51,9 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_user_client_schedule_event";
     "alsaseq_user_client_create_source";
     "alsaseq_user_client_operate_subscription";
+    "alsaseq_user_client_create_queue";
+    "alsaseq_user_client_delete_queue";
+    "alsaseq_user_client_update_queue";
 
     "alsaseq_addr_get_type";
     "alsaseq_addr_new";

src/seq/user-client.h
@@ -12,6 +12,7 @@
 #include <seq/event-fixed.h>
 #include <seq/event-variable.h>
 #include <seq/subscribe-data.h>
+#include <seq/queue-info.h>
 
 G_BEGIN_DECLS
 
@@ -104,6 +105,13 @@ void alsaseq_user_client_operate_subscription(ALSASeqUserClient *self,
                                               ALSASeqSubscribeData *subs_data,
                                               gboolean establish,
                                               GError **error);
+
+void alsaseq_user_client_create_queue(ALSASeqUserClient *self,
+                                ALSASeqQueueInfo *queue_info, GError **error);
+void alsaseq_user_client_delete_queue(ALSASeqUserClient *self,
+                                      guint queue_id, GError **error);
+void alsaseq_user_client_update_queue(ALSASeqUserClient *self,
+                                ALSASeqQueueInfo *queue_info, GError **error);
 
 G_END_DECLS
 

tests/alsaseq-user-client
@@ -26,6 +26,9 @@ methods = (
     'schedule_event',
     'create_source',
     'operate_subscription',
+    'create_queue',
+    'delete_queue',
+    'update_queue',
 )
 signals = ()
 

commit 073a0e7c7903a155165f87066a2238b6f22488a9

seq: user_client: correct ioctl command to update queue
Fixes: 2ab3feab20d3 ("seq: user_client: add APIs to create/delete/update queue")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -607,7 +607,7 @@ void alsaseq_user_client_update_queue(ALSASeqUserClient *self,
 
     seq_queue_info_refer_private(queue_info, &info);
 
-    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_CREATE_QUEUE, info) < 0)
+    if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_SET_QUEUE_INFO, info) < 0)
         generate_error(error, errno);
 }
 