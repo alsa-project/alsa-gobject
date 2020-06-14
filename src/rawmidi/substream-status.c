alsa-project/alsa-gobject

commit 2371b927816de9ba937ff79dcebc5d19b89294e6

rawmidi: substream_status: add properties
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/rawmidi/substream-status.c
@@ -1,11 +1,63 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "substream-status.h"
 
-G_DEFINE_TYPE(ALSARawmidiSubstreamStatus, alsarawmidi_substream_status, G_TYPE_OBJECT)
+#include <sound/asound.h>
+
+struct _ALSARawmidiSubstreamStatusPrivate {
+    struct snd_rawmidi_status status;
+};
+G_DEFINE_TYPE_WITH_PRIVATE(ALSARawmidiSubstreamStatus, alsarawmidi_substream_status, G_TYPE_OBJECT)
+
+enum rawmidi_substream_status_prop_type {
+    RAWMIDI_SUBSTREAM_STATUS_PROP_AVAIL = 1,
+    RAWMIDI_SUBSTREAM_STATUS_PROP_XRUN,
+    RAWMIDI_SUBSTREAM_STATUS_PROP_COUNT,
+};
+static GParamSpec *rawmidi_substream_status_props[RAWMIDI_SUBSTREAM_STATUS_PROP_COUNT] = { NULL, };
+
+static void rawmidi_substream_status_get_property(GObject *obj, guint id,
+                                                GValue *val, GParamSpec *spec)
+{
+    ALSARawmidiSubstreamStatus *self = ALSARAWMIDI_SUBSTREAM_STATUS(obj);
+    ALSARawmidiSubstreamStatusPrivate *priv =
+                        alsarawmidi_substream_status_get_instance_private(self);
+
+    switch (id) {
+    case RAWMIDI_SUBSTREAM_STATUS_PROP_AVAIL:
+        g_value_set_long(val, priv->status.avail);
+        break;
+    case RAWMIDI_SUBSTREAM_STATUS_PROP_XRUN:
+        g_value_set_long(val, priv->status.xruns);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, id, spec);
+        break;
+    }
+}
 
 static void alsarawmidi_substream_status_class_init(ALSARawmidiSubstreamStatusClass *klass)
 {
-    return;
+    GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
+
+    gobject_class->get_property = rawmidi_substream_status_get_property;
+
+    rawmidi_substream_status_props[RAWMIDI_SUBSTREAM_STATUS_PROP_AVAIL] =
+        g_param_spec_ulong("avail", "avail",
+               "The size of available space in intermediate buffer.",
+                          0, G_MAXULONG,
+                          0,
+                          G_PARAM_READABLE);
+
+    rawmidi_substream_status_props[RAWMIDI_SUBSTREAM_STATUS_PROP_XRUN] =
+        g_param_spec_ulong("xruns", "xruns",
+               "The count of XRUNs since the last query of status.",
+                           0, G_MAXULONG,
+                           0,
+                           G_PARAM_READABLE);
+
+    g_object_class_install_properties(gobject_class,
+                                      RAWMIDI_SUBSTREAM_STATUS_PROP_COUNT,
+                                      rawmidi_substream_status_props);
 }
 
 static void alsarawmidi_substream_status_init(ALSARawmidiSubstreamStatus *self)

src/rawmidi/privates.h
@@ -9,7 +9,6 @@
 
 #include "substream-info.h"
 #include "substream-params.h"
-#include "substream-status.h"
 
 #include <sound/asound.h>
 

src/rawmidi/substream-status.h
@@ -31,9 +31,12 @@ G_BEGIN_DECLS
 
 typedef struct _ALSARawmidiSubstreamStatus          ALSARawmidiSubstreamStatus;
 typedef struct _ALSARawmidiSubstreamStatusClass     ALSARawmidiSubstreamStatusClass;
+typedef struct _ALSARawmidiSubstreamStatusPrivate   ALSARawmidiSubstreamStatusPrivate;
 
 struct _ALSARawmidiSubstreamStatus {
     GObject parent_instance;
+
+    ALSARawmidiSubstreamStatusPrivate *priv;
 };
 
 struct _ALSARawmidiSubstreamStatusClass {

tests/alsarawmidi-substream-status
@@ -10,7 +10,10 @@ gi.require_version('ALSARawmidi', '0.0')
 from gi.repository import ALSARawmidi
 
 target = ALSARawmidi.SubstreamStatus()
-props = ()
+props = (
+    'avail',
+    'xruns',
+)
 methods = (
     'new',
 )

commit a746f872bc79450faaebcae27977c6f64f421ff5

rawmidi: substream_status: use uint64_t for avail and xrun properties
The avail and xrun members in struct snd_rawmidi_params are defined as
size_t type. The size_t is long type, thus the size is different between
32 bit ABI and 64 bit ABI. This commit uses uint64_t type for the
properties so that the size is the same as these two ABIs.

Fixes: 2371b927816d ("rawmidi: substream_status: add properties")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/rawmidi/substream-status.c
@@ -34,10 +34,10 @@ static void rawmidi_substream_status_get_property(GObject *obj, guint id,
 
     switch (id) {
     case RAWMIDI_SUBSTREAM_STATUS_PROP_AVAIL:
-        g_value_set_long(val, priv->status.avail);
+        g_value_set_uint64(val, priv->status.avail);
         break;
     case RAWMIDI_SUBSTREAM_STATUS_PROP_XRUN:
-        g_value_set_long(val, priv->status.xruns);
+        g_value_set_uint64(val, priv->status.xruns);
         break;
     default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, id, spec);
@@ -52,18 +52,18 @@ static void alsarawmidi_substream_status_class_init(ALSARawmidiSubstreamStatusCl
     gobject_class->get_property = rawmidi_substream_status_get_property;
 
     rawmidi_substream_status_props[RAWMIDI_SUBSTREAM_STATUS_PROP_AVAIL] =
-        g_param_spec_ulong("avail", "avail",
-               "The size of available space in intermediate buffer.",
-                          0, G_MAXULONG,
-                          0,
-                          G_PARAM_READABLE);
+        g_param_spec_uint64("avail", "avail",
+                            "The size of available space in intermediate buffer.",
+                            0, G_MAXUINT64,
+                            0,
+                            G_PARAM_READABLE);
 
     rawmidi_substream_status_props[RAWMIDI_SUBSTREAM_STATUS_PROP_XRUN] =
-        g_param_spec_ulong("xruns", "xruns",
-               "The count of XRUNs since the last query of status.",
-                           0, G_MAXULONG,
-                           0,
-                           G_PARAM_READABLE);
+        g_param_spec_uint64("xruns", "xruns",
+                            "The count of XRUNs since the last query of status.",
+                            0, G_MAXUINT64,
+                            0,
+                            G_PARAM_READABLE);
 
     g_object_class_install_properties(gobject_class,
                                       RAWMIDI_SUBSTREAM_STATUS_PROP_COUNT,