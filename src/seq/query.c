alsa-project/alsa-gobject

commit 1550112567c6f7c8678608e664d1fbfb95d135de

seq: use guint8 for the numerical ID of queue

src/seq/query.c
@@ -646,7 +646,7 @@ void alsaseq_get_queue_id_list(guint **entries, gsize *entry_count,
  * with SNDRV_SEQ_IOCTL_GET_QUEUE_INFO command for ALSA sequencer character
  * device.
  */
-void alsaseq_get_queue_info_by_id(guint queue_id, ALSASeqQueueInfo **queue_info,
+void alsaseq_get_queue_info_by_id(guint8 queue_id, ALSASeqQueueInfo **queue_info,
                                   GError **error)
 {
     struct snd_seq_queue_info *info;
@@ -736,7 +736,7 @@ void alsaseq_get_queue_info_by_name(const gchar *name,
  * with SNDRV_SEQ_IOCTL_GET_QUEUE_STATUS command for ALSA sequencer character
  * device.
  */
-void alsaseq_get_queue_status(guint queue_id,
+void alsaseq_get_queue_status(guint8 queue_id,
                               ALSASeqQueueStatus *const *queue_status,
                               GError **error)
 {

src/seq/query.h
@@ -43,13 +43,13 @@ void alsaseq_get_subscription_list(const ALSASeqAddr *addr,
 void alsaseq_get_queue_id_list(guint **entries, gsize *entry_count,
                                GError **error);
 
-void alsaseq_get_queue_info_by_id(guint queue_id, ALSASeqQueueInfo **queue_info,
+void alsaseq_get_queue_info_by_id(guint8 queue_id, ALSASeqQueueInfo **queue_info,
                                   GError **error);
 void alsaseq_get_queue_info_by_name(const gchar *name,
                                     ALSASeqQueueInfo **queue_info,
                                     GError **error);
 
-void alsaseq_get_queue_status(guint queue_id,
+void alsaseq_get_queue_status(guint8 queue_id,
                               ALSASeqQueueStatus *const *queue_status,
                               GError **error);
 

src/seq/queue-info.c
@@ -38,7 +38,7 @@ static void seq_queue_info_set_property(GObject *obj, guint id,
 
     switch (id) {
     case SEQ_QUEUE_INFO_PROP_QUEUE_ID:
-        priv->info.queue = g_value_get_int(val);
+        priv->info.queue = (int)g_value_get_uchar(val);
         break;
     case SEQ_QUEUE_INFO_PROP_CLIENT_ID:
         priv->info.owner = g_value_get_int(val);
@@ -64,7 +64,7 @@ static void seq_queue_info_get_property(GObject *obj, guint id, GValue *val,
 
     switch (id) {
     case SEQ_QUEUE_INFO_PROP_QUEUE_ID:
-        g_value_set_int(val, priv->info.queue);
+        g_value_set_uchar(val, (guint8)priv->info.queue);
         break;
     case SEQ_QUEUE_INFO_PROP_CLIENT_ID:
         g_value_set_int(val, priv->info.owner);
@@ -89,12 +89,12 @@ static void alsaseq_queue_info_class_init(ALSASeqQueueInfoClass *klass)
     gobject_class->get_property = seq_queue_info_get_property;
 
     seq_queue_info_props[SEQ_QUEUE_INFO_PROP_QUEUE_ID] =
-        g_param_spec_int("queue-id", "queue-id",
-                         "The numerical ID of queue, except for one of "
-                         "ALSASeqSpecificClientId.",
-                         G_MININT, G_MAXINT,
-                         -1,
-                         G_PARAM_READWRITE);
+        g_param_spec_uchar("queue-id", "queue-id",
+                           "The numerical ID of queue, except for one of "
+                           "ALSASeqSpecificClientId.",
+                           0, G_MAXUINT8,
+                           0,
+                           G_PARAM_READWRITE);
 
     seq_queue_info_props[SEQ_QUEUE_INFO_PROP_CLIENT_ID] =
         g_param_spec_int("client-id", "client-id",

src/seq/queue-status.c
@@ -55,12 +55,12 @@ static void alsaseq_queue_status_class_init(ALSASeqQueueStatusClass *klass)
     gobject_class->get_property = seq_queue_status_get_property;
 
     seq_queue_status_props[SEQ_QUEUE_STATUS_PROP_QUEUE_ID] =
-        g_param_spec_int("queue-id", "queue-id",
-                         "The numerical ID of queue, except for entries in "
-                         "ALSASeqSpecificQueueId.",
-                         G_MININT, G_MAXINT,
-                         -1,
-                         G_PARAM_READABLE);
+        g_param_spec_uchar("queue-id", "queue-id",
+                           "The numerical ID of queue, except for entries in "
+                           "ALSASeqSpecificQueueId.",
+                           0, G_MAXUINT8,
+                           0,
+                           G_PARAM_READABLE);
 
     seq_queue_status_props[SEQ_QUEUE_STATUS_PROP_EVENT_COUNT] =
         g_param_spec_int("event-count", "event-count",

src/seq/queue-tempo.c
@@ -33,7 +33,7 @@ static void seq_queue_tempo_set_property(GObject *obj, guint id,
 
     switch (id) {
     case SEQ_QUEUE_TEMPO_PROP_QUEUE_ID:
-        priv->tempo.queue = g_value_get_int(val);
+        priv->tempo.queue = (int)g_value_get_uchar(val);
         break;
     case SEQ_QUEUE_TEMPO_PROP_TEMPO:
         priv->tempo.tempo = g_value_get_int(val);
@@ -56,7 +56,7 @@ static void seq_queue_tempo_get_property(GObject *obj, guint id, GValue *val,
 
     switch (id) {
     case SEQ_QUEUE_TEMPO_PROP_QUEUE_ID:
-        g_value_set_int(val, priv->tempo.queue);
+        g_value_set_uchar(val, (guint8)priv->tempo.queue);
         break;
     case SEQ_QUEUE_TEMPO_PROP_TEMPO:
         g_value_set_uint(val, priv->tempo.tempo);
@@ -78,12 +78,12 @@ static void alsaseq_queue_tempo_class_init(ALSASeqQueueTempoClass *klass)
     gobject_class->get_property = seq_queue_tempo_get_property;
 
     seq_queue_tempo_props[SEQ_QUEUE_TEMPO_PROP_QUEUE_ID] =
-        g_param_spec_int("queue-id", "queue-id",
-                         "The numerical ID of queue, except for one of "
-                         "ALSASeqSpecificClientId.",
-                         G_MININT, G_MAXINT,
-                         -1,
-                         G_PARAM_READWRITE);
+        g_param_spec_uchar("queue-id", "queue-id",
+                           "The numerical ID of queue, except for one of "
+                           "ALSASeqSpecificClientId.",
+                           0, G_MAXUINT8,
+                           0,
+                           G_PARAM_READWRITE);
 
     seq_queue_tempo_props[SEQ_QUEUE_TEMPO_PROP_TEMPO] =
         g_param_spec_uint("tempo", "tempo",

src/seq/queue-timer.c
@@ -52,12 +52,12 @@ static void alsaseq_queue_timer_class_init(ALSASeqQueueTimerClass *klass)
     gobject_class->get_property = seq_queue_timer_get_property;
 
     seq_queue_timer_props[SEQ_QUEUE_TIMER_PROP_QUEUE_ID] =
-        g_param_spec_int("queue-id", "queue-id",
-                         "The numerical ID of queue, except for one of "
-                         "ALSASeqSpecificClientId.",
-                         G_MININT, G_MAXINT,
-                         -1,
-                         G_PARAM_READABLE);
+        g_param_spec_uchar("queue-id", "queue-id",
+                           "The numerical ID of queue, except for one of "
+                           "ALSASeqSpecificClientId.",
+                           0, G_MAXUINT8,
+                           0,
+                           G_PARAM_READABLE);
 
     seq_queue_timer_props[SEQ_QUEUE_TIMER_PROP_TIMER_TYPE] =
         g_param_spec_enum("type", "type",

src/seq/user-client.c
@@ -639,7 +639,7 @@ void alsaseq_user_client_create_queue(ALSASeqUserClient *self,
  * SNDRV_SEQ_IOCTL_DELETE_QUEUE command for ALSA sequencer character device.
  */
 void alsaseq_user_client_delete_queue(ALSASeqUserClient *self,
-                                      guint queue_id, GError **error)
+                                      guint8 queue_id, GError **error)
 {
     ALSASeqUserClientPrivate *priv;
     struct snd_seq_queue_info info = {0};
@@ -695,7 +695,7 @@ void alsaseq_user_client_update_queue(ALSASeqUserClient *self,
  * SNDRV_SEQ_IOCTL_GET_QUEUE_CLIENT command for ALSA sequencer character device.
  */
 void alsaseq_user_client_get_queue_usage(ALSASeqUserClient *self,
-                                         guint queue_id, gboolean *use,
+                                         guint8 queue_id, gboolean *use,
                                          GError **error)
 {
     ALSASeqUserClientPrivate *priv;
@@ -727,7 +727,7 @@ void alsaseq_user_client_get_queue_usage(ALSASeqUserClient *self,
  * SNDRV_SEQ_IOCTL_SET_QUEUE_CLIENT command for ALSA sequencer character device.
  */
 void alsaseq_user_client_set_queue_usage(ALSASeqUserClient *self,
-                                         guint queue_id, gboolean use,
+                                         guint8 queue_id, gboolean use,
                                          GError **error)
 {
     ALSASeqUserClientPrivate *priv;
@@ -758,7 +758,7 @@ void alsaseq_user_client_set_queue_usage(ALSASeqUserClient *self,
  * SNDRV_SEQ_IOCTL_SET_QUEUE_TEMPO command for ALSA sequencer character device.
  */
 void alsaseq_user_client_set_queue_tempo(ALSASeqUserClient *self,
-                                guint queue_id, ALSASeqQueueTempo *queue_tempo,
+                                guint8 queue_id, ALSASeqQueueTempo *queue_tempo,
                                 GError **error)
 {
     ALSASeqUserClientPrivate *priv;
@@ -788,7 +788,7 @@ void alsaseq_user_client_set_queue_tempo(ALSASeqUserClient *self,
  * SNDRV_SEQ_IOCTL_GET_QUEUE_TEMPO command for ALSA sequencer character device.
  */
 void alsaseq_user_client_get_queue_tempo(ALSASeqUserClient *self,
-                                guint queue_id, ALSASeqQueueTempo **queue_tempo,
+                                guint8 queue_id, ALSASeqQueueTempo **queue_tempo,
                                 GError **error)
 {
     ALSASeqUserClientPrivate *priv;
@@ -822,7 +822,7 @@ void alsaseq_user_client_get_queue_tempo(ALSASeqUserClient *self,
  * SNDRV_SEQ_IOCTL_SET_QUEUE_TIMER command for ALSA sequencer character device.
  */
 void alsaseq_user_client_set_queue_timer(ALSASeqUserClient *self,
-                                         guint queue_id,
+                                         guint8 queue_id,
                                          ALSASeqQueueTimer *queue_timer,
                                          GError **error)
 {
@@ -864,7 +864,7 @@ void alsaseq_user_client_set_queue_timer(ALSASeqUserClient *self,
  * SNDRV_SEQ_IOCTL_GET_QUEUE_TIMER command for ALSA sequencer character device.
  */
 void alsaseq_user_client_get_queue_timer(ALSASeqUserClient *self,
-                                         guint queue_id,
+                                         guint8 queue_id,
                                          ALSASeqQueueTimer **queue_timer,
                                          GError **error)
 {

src/seq/user-client.h
@@ -119,30 +119,30 @@ void alsaseq_user_client_create_queue(ALSASeqUserClient *self,
                                       ALSASeqQueueInfo *const *queue_info,
                                       GError **error);
 void alsaseq_user_client_delete_queue(ALSASeqUserClient *self,
-                                      guint queue_id, GError **error);
+                                      guint8 queue_id, GError **error);
 void alsaseq_user_client_update_queue(ALSASeqUserClient *self,
                                 ALSASeqQueueInfo *queue_info, GError **error);
 
 void alsaseq_user_client_get_queue_usage(ALSASeqUserClient *self,
-                                         guint queue_id, gboolean *use,
+                                         guint8 queue_id, gboolean *use,
                                          GError **error);
 void alsaseq_user_client_set_queue_usage(ALSASeqUserClient *self,
-                                         guint queue_id, gboolean use,
+                                         guint8 queue_id, gboolean use,
                                          GError **error);
 
 void alsaseq_user_client_set_queue_tempo(ALSASeqUserClient *self,
-                                guint queue_id, ALSASeqQueueTempo *queue_tempo,
+                                guint8 queue_id, ALSASeqQueueTempo *queue_tempo,
                                 GError **error);
 void alsaseq_user_client_get_queue_tempo(ALSASeqUserClient *self,
-                                guint queue_id, ALSASeqQueueTempo **queue_tempo,
+                                guint8 queue_id, ALSASeqQueueTempo **queue_tempo,
                                 GError **error);
 
 void alsaseq_user_client_set_queue_timer(ALSASeqUserClient *self,
-                                         guint queue_id,
+                                         guint8 queue_id,
                                          ALSASeqQueueTimer *queue_timer,
                                          GError **error);
 void alsaseq_user_client_get_queue_timer(ALSASeqUserClient *self,
-                                         guint queue_id,
+                                         guint8 queue_id,
                                          ALSASeqQueueTimer **queue_timer,
                                          GError **error);
 

commit 874107705b7af8e94d53a15202b0557af9890a47

seq: supplement to use guint8 for the numerical ID of queue
Fixes: 1550112567c6 ("seq: use guint8 for the numerical ID of queue")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/query.c
@@ -567,7 +567,7 @@ void alsaseq_get_subscription_list(const ALSASeqAddr *addr,
  * with SNDRV_SEQ_IOCTL_SYSTEM_INFO and SNDRV_SEQ_IOCTL_GET_QUEUE_INFO commands
  * for ALSA sequencer character device.
  */
-void alsaseq_get_queue_id_list(guint **entries, gsize *entry_count,
+void alsaseq_get_queue_id_list(guint8 **entries, gsize *entry_count,
                                GError **error)
 {
     char *devnode;
@@ -575,7 +575,7 @@ void alsaseq_get_queue_id_list(guint **entries, gsize *entry_count,
     struct snd_seq_system_info info = {0};
     unsigned int maximum_count;
     unsigned int count;
-    guint *list;
+    guint8 *list;
     unsigned int index;
     int i;
 

src/seq/query.h
@@ -40,7 +40,7 @@ void alsaseq_get_subscription_list(const ALSASeqAddr *addr,
                                    ALSASeqQuerySubscribeType query_type,
                                    GList **entries, GError **error);
 
-void alsaseq_get_queue_id_list(guint **entries, gsize *entry_count,
+void alsaseq_get_queue_id_list(guint8 **entries, gsize *entry_count,
                                GError **error);
 
 void alsaseq_get_queue_info_by_id(guint8 queue_id, ALSASeqQueueInfo **queue_info,