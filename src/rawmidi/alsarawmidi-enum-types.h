alsa-project/alsa-gobject

commit 664d41f58fcd337f94e135ec522091c035ca95a0

rawmidi: generate enumerations
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/rawmidi/alsarawmidi-enum-types.h
@@ -1,0 +1,34 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSARAWMIDI_ENUM_TYPES__H__
+#define __ALSA_GOBJECT_ALSARAWMIDI_ENUM_TYPES__H__
+
+#include <sound/asound.h>
+
+/**
+ * ALSARawmidiStreamDirection:
+ * @ALSARAWMIDI_STREAM_DIRECTION_OUTPUT:	Output direction of stream.
+ * @ALSARAWMIDI_STREAM_DIRECTION_INPUT:		Input direction of stream.
+ *
+ * A set of enumerators for the direction of stream.
+ */
+typedef enum {
+    ALSARAWMIDI_STREAM_DIRECTION_OUTPUT = SNDRV_RAWMIDI_STREAM_OUTPUT,
+    ALSARAWMIDI_STREAM_DIRECTION_INPUT = SNDRV_RAWMIDI_STREAM_INPUT,
+} ALSARawmidiStreamDirection;
+
+/**
+ * ALSARawmidiStreamPairInfoFlag:
+ * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_OUTPUT:	The pair of stream supports output substream.
+ * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_INPUT:	The pair of stream supports input substream.
+ * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_DUPLEX:	Both directions of stream are available at the same time.
+ *
+ * A set of flags for information of the pair of streams.
+ */
+typedef enum /*< flags >*/
+{
+    ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_OUTPUT = SNDRV_RAWMIDI_INFO_OUTPUT,
+    ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_INPUT  = SNDRV_RAWMIDI_INFO_INPUT,
+    ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_DUPLEX = SNDRV_RAWMIDI_INFO_DUPLEX,
+} ALSARawmidiStreamPairInfoFlag;
+
+#endif

src/rawmidi/alsarawmidi.map
@@ -1,4 +1,7 @@
 ALSA_GOBJECT_0_0_0 {
+  global:
+    "alsarawmidi_stream_direction_get_type";
+    "alsarawmidi_stream_pair_info_flag_get_type";
   local:
     *;
 };

src/rawmidi/meson.build
@@ -5,7 +5,7 @@
 path = 'rawmidi'
 name = 'alsarawmidi'
 namespace = 'ALSARawmidi'
-has_enumerations = false
+has_enumerations = true
 has_marshaller = false
 
 sources = files()

commit 91d3beedf629528e76d8685bd1a64632b50d0272

rawmidi: fix indentation
Fixes: 664d41f58fcd ("rawmidi: generate enumerations")

src/rawmidi/alsarawmidi-enum-types.h
@@ -6,8 +6,8 @@
 
 /**
  * ALSARawmidiStreamDirection:
- * @ALSARAWMIDI_STREAM_DIRECTION_OUTPUT:	Output direction of stream.
- * @ALSARAWMIDI_STREAM_DIRECTION_INPUT:		Input direction of stream.
+ * @ALSARAWMIDI_STREAM_DIRECTION_OUTPUT:        Output direction of stream.
+ * @ALSARAWMIDI_STREAM_DIRECTION_INPUT:         Input direction of stream.
  *
  * A set of enumerators for the direction of stream.
  */
@@ -18,9 +18,9 @@ typedef enum {
 
 /**
  * ALSARawmidiStreamPairInfoFlag:
- * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_OUTPUT:	The pair of stream supports output substream.
- * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_INPUT:	The pair of stream supports input substream.
- * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_DUPLEX:	Both directions of stream are available at the same time.
+ * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_OUTPUT:   The pair of stream supports output substream.
+ * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_INPUT:    The pair of stream supports input substream.
+ * @ALSARAWMIDI_STREAM_PAIR_INFO_FLAG_DUPLEX:   Both directions of stream are available at the same time.
  *
  * A set of flags for information of the pair of streams.
  */