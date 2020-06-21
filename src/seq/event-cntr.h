alsa-project/alsa-gobject

commit 7e43bc934ab54a81ccd798d326def991d51305a1

seq: event_cntr: add an API to calculate the amount of cells in pool consumed by a batch of event
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-cntr.h
@@ -49,6 +49,9 @@ ALSASeqEventCntr *alsaseq_event_cntr_new(guint count, GError **error);
 
 void alsaseq_event_cntr_count_events(ALSASeqEventCntr *self, gsize *count);
 
+void alsaseq_event_cntr_calculate_pool_consumption(ALSASeqEventCntr *self,
+				gsize count, gsize *cells, GError **error);
+
 G_END_DECLS
 
 #endif

src/seq/alsaseq.map
@@ -173,6 +173,7 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_event_cntr_get_type";
     "alsaseq_event_cntr_new";
     "alsaseq_event_cntr_count_events";
+    "alsaseq_event_cntr_calculate_pool_consumption";
   local:
     *;
 };

src/seq/event-cntr.c
@@ -153,3 +153,37 @@ void alsaseq_event_cntr_count_events(ALSASeqEventCntr *self, gsize *count)
     while ((ev = event_iterator_next(&iter)))
         ++(*count);
 }
+
+/**
+ * alsaseq_event_cntr_calculate_pool_consumption:
+ * @self: A #ALSASeqEventCntr.
+ * @count: The amount of events for calculation.
+ * @cells: (out): The amount of cells to be consumed in pool.
+ * @error: A #GError.
+ *
+ * Calculate the amount of cells in client pool to be consumed by a part
+ * of events in the container.
+ */
+void alsaseq_event_cntr_calculate_pool_consumption(ALSASeqEventCntr *self,
+                                    gsize count, gsize *cells, GError **error)
+{
+    ALSASeqEventCntrPrivate *priv;
+    struct event_iterator iter;
+    struct snd_seq_event *ev;
+    gsize total;
+
+    g_return_if_fail(ALSASEQ_IS_EVENT_CNTR(self));
+    priv = alsaseq_event_cntr_get_instance_private(self);
+
+    *cells = 0;
+    total = 0;
+    event_iterator_init(&iter, priv->buf, priv->length, priv->allocated);
+    while ((ev = event_iterator_next(&iter))) {
+        gsize size = calculate_event_size(ev, TRUE);
+        *cells += size /sizeof(*ev);
+
+        if (total == count)
+            break;
+        ++total;
+    }
+}

tests/alsaseq-event-cntr
@@ -14,6 +14,7 @@ props = ()
 methods = (
     'new',
     'count_events',
+    'calculate_pool_consumption',
 )
 signals = ()
 

commit 9d7b956228fdd1d8ee3269be193d47e79543c896

seq: event_cntr: fix indentation
Fixes: 7e43bc934ab5 ("seq: event_cntr: add an API to calculate the amount of cells in pool consumed by a batch of event")

src/seq/event-cntr.h
@@ -59,7 +59,7 @@ ALSASeqEventCntr *alsaseq_event_cntr_new(guint count, GError **error);
 void alsaseq_event_cntr_count_events(ALSASeqEventCntr *self, gsize *count);
 
 void alsaseq_event_cntr_calculate_pool_consumption(ALSASeqEventCntr *self,
-				gsize count, gsize *cells, GError **error);
+                                    gsize count, gsize *cells, GError **error);
 
 void alsaseq_event_cntr_get_event_type(ALSASeqEventCntr *self, gsize index,
                                     ALSASeqEventType *ev_type, GError **error);