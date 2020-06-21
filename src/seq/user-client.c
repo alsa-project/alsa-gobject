alsa-project/alsa-gobject

commit 3bbab4321c58677b03cf4adbf4161c6f2242bd7f

seq: user_client: return filled object after creating queue
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/user-client.c
@@ -601,7 +601,7 @@ void alsaseq_user_client_operate_subscription(ALSASeqUserClient *self,
 /**
  * alsaseq_user_client_create_queue:
  * @self: A #ALSASeqUserClient.
- * @queue_info: The information of queue to add.
+ * @queue_info: (inout): The information of queue to add.
  * @error: A #GError.
  *
  * Create a new queue owned by the client. The content of information is updated
@@ -611,16 +611,17 @@ void alsaseq_user_client_operate_subscription(ALSASeqUserClient *self,
  * SNDRV_SEQ_IOCTL_CREATE_QUEUE command for ALSA sequencer character device.
  */
 void alsaseq_user_client_create_queue(ALSASeqUserClient *self,
-                                ALSASeqQueueInfo *queue_info, GError **error)
+                                      ALSASeqQueueInfo *const *queue_info,
+				      GError **error)
 {
     ALSASeqUserClientPrivate *priv;
     struct snd_seq_queue_info *info;
 
     g_return_if_fail(ALSASEQ_IS_USER_CLIENT(self));
-    g_return_if_fail(ALSASEQ_IS_QUEUE_INFO(queue_info));
+    g_return_if_fail(ALSASEQ_IS_QUEUE_INFO(*queue_info));
     priv = alsaseq_user_client_get_instance_private(self);
 
-    seq_queue_info_refer_private(queue_info, &info);
+    seq_queue_info_refer_private(*queue_info, &info);
 
     if (ioctl(priv->fd, SNDRV_SEQ_IOCTL_CREATE_QUEUE, info) < 0)
         generate_error(error, errno);

src/seq/user-client.h
@@ -116,7 +116,8 @@ void alsaseq_user_client_operate_subscription(ALSASeqUserClient *self,
                                               GError **error);
 
 void alsaseq_user_client_create_queue(ALSASeqUserClient *self,
-                                ALSASeqQueueInfo *queue_info, GError **error);
+                                      ALSASeqQueueInfo *const *queue_info,
+                                      GError **error);
 void alsaseq_user_client_delete_queue(ALSASeqUserClient *self,
                                       guint queue_id, GError **error);
 void alsaseq_user_client_update_queue(ALSASeqUserClient *self,

commit db7d12ba42512e7391bed686b911c1677d75b0db

seq: user_client: fix indentation
Fixes: 3bbab4321c58 ("seq: user_client: return filled object after creating queue")

src/seq/user-client.c
@@ -612,7 +612,7 @@ void alsaseq_user_client_operate_subscription(ALSASeqUserClient *self,
  */
 void alsaseq_user_client_create_queue(ALSASeqUserClient *self,
                                       ALSASeqQueueInfo *const *queue_info,
-				      GError **error)
+                                      GError **error)
 {
     ALSASeqUserClientPrivate *priv;
     struct snd_seq_queue_info *info;