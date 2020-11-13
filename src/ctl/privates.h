da4e0e79455c433191b57d730b1e24491ea4d48e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-11-13 16:26:26 +0900

    ctl: obsolete library-wide GQuark for error reporting
    This commit obsoletes library-wide GQuark. This is not exported, thus
    it doesn't lose backward compatibility.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/ctl/privates.h
+++ src/ctl/privates.h
@@ -16,12 +16,6 @@
 
 G_BEGIN_DECLS
 
-GQuark alsactl_error_quark(void);
-
-#define generate_error(err, errno)                      \
-    g_set_error(err, alsactl_error_quark(), errno,      \
-                __FILE__ ":%d: %s", __LINE__, strerror(errno))
-
 void ctl_card_info_refer_private(ALSACtlCardInfo *self,
                                  struct snd_ctl_card_info **info);
 
