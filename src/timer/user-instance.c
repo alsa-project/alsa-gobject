alsa-project/alsa-gobject

commit 3a6d788bf47d31f08f5f699c96b60f720bf33421

timer: user_instance: add an API to attach user instance to another instance as slave
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/timer/user-instance.c
@@ -96,3 +96,36 @@ void alsatimer_user_instance_attach(ALSATimerUserInstance *self,
     if (ioctl(priv->fd, SNDRV_TIMER_IOCTL_SELECT, &sel) < 0)
         generate_error(error, errno);
 }
+
+/**
+ * alsatimer_user_instance_attach_as_slave:
+ * @self: A #ALSATimerUserInstance.
+ * @slave_class: The class identifier of master instance, one of
+ *               #ALSATimerSlaveClass.
+ * @slave_id: The numerical identifier of master instance.
+ * @error: A #GError.
+ *
+ * Attach the instance to timer device as an slave to another instance indicated
+ * by a pair of slave_class and slave_id. If the slave_class is for application
+ * (=ALSATIMER_SLAVE_CLASS_APPLICATION), the slave_id is for the PID of
+ * application process which owns the instance of timer. If the slave_class is
+ * for ALSA sequencer (=ALSATIMER_SLAVE_CLASS_SEQUENCER), the slave_id is the
+ * numerical ID of queue bound for timer device.
+ */
+void alsatimer_user_instance_attach_as_slave(ALSATimerUserInstance *self,
+                                        ALSATimerSlaveClass slave_class,
+                                        int slave_id,
+                                        GError **error)
+{
+    ALSATimerUserInstancePrivate *priv;
+    struct snd_timer_select sel = {0};
+
+    g_return_if_fail(ALSATIMER_IS_USER_INSTANCE(self));
+    priv = alsatimer_user_instance_get_instance_private(self);
+
+    sel.id.dev_class = SNDRV_TIMER_CLASS_SLAVE;
+    sel.id.dev_sclass = slave_class;
+    sel.id.device = slave_id;
+    if (ioctl(priv->fd, SNDRV_TIMER_IOCTL_SELECT, &sel) < 0)
+        generate_error(error, errno);
+}

src/timer/user-instance.h
@@ -55,6 +55,11 @@ void alsatimer_user_instance_attach(ALSATimerUserInstance *self,
                                     ALSATimerDeviceId *device_id,
                                     GError **error);
 
+void alsatimer_user_instance_attach_as_slave(ALSATimerUserInstance *self,
+                                        ALSATimerSlaveClass slave_class,
+                                        int slave_id,
+                                        GError **error);
+
 G_END_DECLS
 
 #endif

tests/alsatimer-user-instance
@@ -15,6 +15,7 @@ methods = (
     'new',
     'open',
     'attach',
+    'attach_as_slave',
 )
 signals = ()
 

commit bea634a220fa4235cc9b2b2b185f0fb1d648af18

timer: user_instance: fix function comment
The function comment for documentation includes wrong descriptions.

This commit corrects it.

Fixes: 3a6d788bf47d ("timer: user_instance: add an API to attach user instance to another instance as slave")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/timer/user-instance.c
@@ -254,15 +254,12 @@ void alsatimer_user_instance_attach(ALSATimerUserInstance *self,
  * @slave_id: The numerical identifier of master instance.
  * @error: A #GError.
  *
- * Attach the instance to timer device as an slave to another instance indicated
- * by a pair of slave_class and slave_id. If the slave_class is for application
+ * Attach the instance as an slave to another instance indicated by a pair of
+ * slave_class and slave_id. If the slave_class is for application
  * (=ALSATIMER_SLAVE_CLASS_APPLICATION), the slave_id is for the PID of
  * application process which owns the instance of timer. If the slave_class is
  * for ALSA sequencer (=ALSATIMER_SLAVE_CLASS_SEQUENCER), the slave_id is the
  * numerical ID of queue bound for timer device.
- *
- * Attach the instance to the timer device. If the given device_id is for
- * absent timer device, the instance can be detached with error.
  *
  * The call of function executes ioctl(2) system call with
  * SNDRV_TIMER_IOCTL_SELECT command for ALSA timer character device.