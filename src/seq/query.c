alsa-project/alsa-gobject

commit c5b7c598fde46a48d4334ccc529dbc8bc1a3ec80

seq: add global method to get list of client ID in ALSA Sequencer
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/query.c
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <stdbool.h>
 
 #include <libudev.h>
 
@@ -140,4 +141,87 @@ void alsaseq_get_system_info(ALSASeqSystemInfo **system_info, GError **error)
 
     // Decrement count for the above connection.
     --info->cur_clients;
+}
+
+/**
+ * alsaseq_get_client_id_list:
+ * @entries: (array length=entry_count)(out): The array with elements for
+ *           numerical ID of client. One of ALSASeqSpecificClientId can be
+ *           included in result as well as any numerical value.
+ * @entry_count: The number of entries.
+ * @error: A #GError.
+ *
+ * Get the list of clients as the numerical ID.
+ */
+void alsaseq_get_client_id_list(guint **entries, gsize *entry_count,
+                                GError **error)
+{
+    char *devnode;
+    int my_id;
+    struct snd_seq_system_info system_info = {0};
+    unsigned int count;
+    guint *list;
+    unsigned int index;
+    struct snd_seq_client_info client_info = {0};
+    int fd;
+
+    alsaseq_get_seq_devnode(&devnode, error);
+    if (*error != NULL)
+        return;
+
+    fd = open(devnode, O_RDONLY);
+    g_free(devnode);
+    if (fd < 0) {
+        generate_error(error, errno);
+        return;
+    }
+
+    if (ioctl(fd, SNDRV_SEQ_IOCTL_CLIENT_ID, &my_id) < 0) {
+        generate_error(error, errno);
+        close(fd);
+        return;
+    }
+
+    if (ioctl(fd, SNDRV_SEQ_IOCTL_SYSTEM_INFO, &system_info) < 0) {
+        generate_error(error, errno);
+        close(fd);
+        return;
+    }
+
+    // Exclude myself.
+    count = system_info.cur_clients - 1;
+    if (count == 0)  {
+        *entry_count = 0;
+        close(fd);
+        return;
+    }
+
+    list = g_try_malloc0_n(count, sizeof(guint));
+    if (list == NULL) {
+        *entry_count = 0;
+        close(fd);
+        return;
+    }
+    index = 0;
+
+    client_info.client = -1;
+    while (index < count) {
+        if (ioctl(fd, SNDRV_SEQ_IOCTL_QUERY_NEXT_CLIENT, &client_info) < 0)
+            break;
+
+        if (client_info.client != my_id) {
+            list[index] = client_info.client;
+            ++index;
+        }
+    }
+    close(fd);
+
+    if (index != count) {
+        generate_error(error, ENXIO);
+        g_free(list);
+        return;
+    }
+
+    *entries = list;
+    *entry_count = count;
 }

src/seq/alsaseq.map
@@ -19,6 +19,7 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_get_seq_sysname";
     "alsaseq_get_seq_devnode";
     "alsaseq_get_system_info";
+    "alsaseq_get_client_id_list";
 
     "alsaseq_system_info_get_type";
   local:

src/seq/query.h
@@ -15,6 +15,9 @@ void alsaseq_get_seq_devnode(gchar **devnode, GError **error);
 
 void alsaseq_get_system_info(ALSASeqSystemInfo **system_info, GError **error);
 
+void alsaseq_get_client_id_list(guint **entries, gsize *entry_count,
+                                GError **error);
+
 G_END_DECLS
 
 #endif

commit 0f59d584084813ff4c4436a14e596c63334b4466

seq: query: remove duplicated semicolon
Fixes: c5b7c598fde4 ("seq: add global method to get list of client ID in ALSA Sequencer")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/query.c
@@ -246,7 +246,7 @@ void alsaseq_get_client_id_list(guint8 **entries, gsize *entry_count,
 
     if (*error != NULL) {
         g_free(list);
-        goto end;;
+        goto end;
     }
 
     g_warn_if_fail(index == count);