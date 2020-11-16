513b5cd543a51ecb61562fb2c7fac932a756887a
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-11-16 11:45:41 +0900

    rawmidi: obsolete library-wide GQuark for error reporting
    This commit obsoletes library-wide GQuark. This is not exported, thus
    it doesn't lose backward compatibility.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/rawmidi/privates.h
+++ src/rawmidi/privates.h
@@ -15,12 +15,6 @@
 
 G_BEGIN_DECLS
 
-GQuark alsarawmidi_error_quark(void);
-
-#define generate_error(err, errno)                              \
-    g_set_error(err, alsarawmidi_error_quark(), errno,          \
-                __FILE__ ":%d: %s", __LINE__, strerror(errno))
-
 void rawmidi_substream_info_refer_private(ALSARawmidiSubstreamInfo *self,
                                           struct snd_rawmidi_info **info);
 
