alsa-project/alsa-gobject

commit dcd983281ef8ea319ecb485267b50cd4da2b0703

timer: device_id: add accessor methods and constructors
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/timer/device-id.c
@@ -7,3 +7,80 @@ ALSATimerDeviceId *timer_device_id_copy(const ALSATimerDeviceId *self)
 }
 
 G_DEFINE_BOXED_TYPE(ALSATimerDeviceId, alsatimer_device_id, timer_device_id_copy, g_free)
+
+/**
+ * alsatimer_device_id_new:
+ * @class: The class of device, one of #ALSATImerClass.
+ * @card_id: The numerical ID of relevant sound card.
+ * @device_id: The numerical ID of relevant device.
+ * @subdevice_id: The numerical ID of relevant subdevice.
+ *
+ * Allocate and return an instance of ALSATimerDeviceId.
+ *
+ * Returns: (transfer full): A #ALSATimerDeviceId.
+ */
+ALSATimerDeviceId *alsatimer_device_id_new(ALSATimerClass class,
+                                           gint card_id, gint device_id,
+                                           gint subdevice_id)
+{
+    ALSATimerDeviceId *self = g_malloc0(sizeof(*self));
+
+    self->dev_class= class;
+    self->card = card_id;
+    self->device = device_id;
+    self->subdevice = subdevice_id;
+
+    return self;
+}
+
+/**
+ * alsatimer_device_id_get_class:
+ * @self: A #ALSATimerDeviceId.
+ * @class: (out): The class of timer, one of #ALSATimerClass.
+ *
+ * Get the class of timer.
+ */
+void alsatimer_device_id_get_class(const ALSATimerDeviceId *self,
+                                   ALSATimerClass *class)
+{
+    *class = self->dev_class;
+}
+
+/**
+ * alsatimer_device_id_get_card_id:
+ * @self: A #ALSATimerDeviceId.
+ * @card_id: (out): The numerical ID of sound card to which the timer belongs.
+ *
+ * Get the numerical ID of sound card to which the device belongs.
+ */
+void alsatimer_device_id_get_card_id(const ALSATimerDeviceId *self,
+                                     gint *card_id)
+{
+    *card_id = self->card;
+}
+
+/**
+ * alsatimer_device_id_get_device_id:
+ * @self: A #ALSATimerDeviceId.
+ * @device_id: (out): The numerical ID of device to which the timer belongs.
+ *
+ * Get the numerical ID of device to which the timer belongs.
+ */
+void alsatimer_device_id_get_device_id(const ALSATimerDeviceId *self,
+                                       gint *device_id)
+{
+    *device_id = self->device;
+}
+
+/**
+ * alsatimer_device_id_get_subdevice_id:
+ * @self: A #ALSATimerDeviceId.
+ * @subdevice_id: (out): The numerical ID of subdevice to which the timer belongs.
+ *
+ * Get the numerical ID of subdevice to which the timer belongs.
+ */
+void alsatimer_device_id_get_subdevice_id(const ALSATimerDeviceId *self,
+                                          gint *subdevice_id)
+{
+    *subdevice_id = self->subdevice;
+}

src/timer/alsatimer.map
@@ -11,6 +11,11 @@ ALSA_GOBJECT_0_0_0 {
     "alsatimer_get_devnode";
 
     "alsatimer_device_id_get_type";
+    "alsatimer_device_id_new";
+    "alsatimer_device_id_get_class";
+    "alsatimer_device_id_get_card_id";
+    "alsatimer_device_id_get_device_id";
+    "alsatimer_device_id_get_subdevice_id";
   local:
     *;
 };

src/timer/device-id.h
@@ -5,6 +5,8 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include <timer/alsatimer-enums.h>
+
 #include <sound/asound.h>
 
 G_BEGIN_DECLS
@@ -14,6 +16,22 @@ G_BEGIN_DECLS
 typedef struct snd_timer_id ALSATimerDeviceId;
 
 GType alsatimer_device_id_get_type() G_GNUC_CONST;
+
+ALSATimerDeviceId *alsatimer_device_id_new(ALSATimerClass class,
+                                           gint card_id, gint device_id,
+                                           gint subdevice_id);
+
+void alsatimer_device_id_get_class(const ALSATimerDeviceId *self,
+                                   ALSATimerClass *class);
+
+void alsatimer_device_id_get_card_id(const ALSATimerDeviceId *self,
+                                     gint *card_id);
+
+void alsatimer_device_id_get_device_id(const ALSATimerDeviceId *self,
+                                       gint *device_id);
+
+void alsatimer_device_id_get_subdevice_id(const ALSATimerDeviceId *self,
+                                          gint *subdevice_id);
 
 G_END_DECLS
 

tests/alsatimer-device-id
@@ -1,0 +1,26 @@
+#!/usr/bin/env python3
+
+from sys import exit
+from errno import ENXIO
+
+from helper import test
+
+import gi
+gi.require_version('ALSATimer', '0.0')
+from gi.repository import ALSATimer
+
+target = ALSATimer.DeviceId()
+props = (
+    'class',
+    'slave-class',
+    'card-id',
+    'device-id',
+    'subdevice-id',
+)
+methods = (
+    'new',
+)
+signals = ()
+
+if not test(target, props, methods, signals):
+    exit(ENXIO)

commit f8e5381836ee9698c060798b1f7410c26b029e3c

timer: device_id: fix function comment
Fixes: dcd983281ef8 ("timer: device_id: add accessor methods and constructors")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/timer/device-id.c
@@ -23,7 +23,7 @@ G_DEFINE_BOXED_TYPE(ALSATimerDeviceId, alsatimer_device_id, timer_device_id_copy
 
 /**
  * alsatimer_device_id_new:
- * @class: The class of device, one of #ALSATImerClass.
+ * @class: The class of device, one of #ALSATimerClass.
  * @card_id: The numerical ID of relevant sound card.
  * @device_id: The numerical ID of relevant device.
  * @subdevice_id: The numerical ID of relevant subdevice.