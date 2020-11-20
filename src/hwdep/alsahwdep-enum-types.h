alsa-project/alsa-gobject

commit 843c9fe68228a705ca848dcd19109c149bf7cb1c

hwdep: conditional build for Linux kernel 4.12 or later
The type of interface in ALSA HwDep interface got a new entries in Linux
kernel v4.12.

This commit adds conditional macro to build for Linux kernel v4.12 or later.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hwdep/alsahwdep-enum-types.h
@@ -32,8 +32,8 @@
  * @ALSAHWDEP_IFACE_TYPE_FW_DIGI00X:        For Digidesign Digi 002/003 family.
  * @ALSAHWDEP_IFACE_TYPE_FW_TASCAM:         For TASCAM FireWire series.
  * @ALSAHWDEP_IFACE_TYPE_LINE6:             For Line6 USB processors. Available in Linux kernel 4.9.0 or later.
- * @ALSAHWDEP_IFACE_TYPE_FW_MOTU:           For MOTU FireWire series.
- * @ALSAHWDEP_IFACE_TYPE_FW_FIREFACE:       For RME Fireface series.
+ * @ALSAHWDEP_IFACE_TYPE_FW_MOTU:           For MOTU FireWire series. Available in Linux kernel 4.12.0 or later.
+ * @ALSAHWDEP_IFACE_TYPE_FW_FIREFACE:       For RME Fireface series. Available in Linux kernel 4.12.0 or later.
  *
  * A set of enumerators for the interface of hwdep device.
  */
@@ -67,8 +67,13 @@ typedef enum {
 #else
     ALSAHWDEP_IFACE_TYPE_LINE6,
 #endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0)
     ALSAHWDEP_IFACE_TYPE_FW_MOTU        = SNDRV_HWDEP_IFACE_FW_MOTU,
     ALSAHWDEP_IFACE_TYPE_FW_FIREFACE    = SNDRV_HWDEP_IFACE_FW_FIREFACE,
+#else
+    ALSAHWDEP_IFACE_TYPE_FW_MOTU,
+    ALSAHWDEP_IFACE_TYPE_FW_FIREFACE,
+#endif
 } ALSAHwdepIfaceType;
 
 #endif

commit 14d9c8ae05c38eb3cbf2ee3be5e2027632a3e010

hwdep: fix metadata and documentation for the type of interface
The scanner of g-i and gtk-doc can't handle macro lines in enumeration
lines expectedly.

This commit fixes to generate valid metadata and documentation.

Fixes: 843c9fe68228 ("hwdep: conditional build for Linux kernel 4.12 or later")
Fixes: 45f650bf29e8 ("hwdep: conditional build for Linux kernel v4.9 or later")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hwdep/alsahwdep-enum-types.h
@@ -5,6 +5,19 @@
 #include <sound/asound.h>
 #include <linux/version.h>
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,9,0)
+#ifndef __GTK_DOC_IGNORE__
+#define SNDRV_HWDEP_IFACE_LINE6         (SNDRV_HWDEP_IFACE_FW_TASCAM + 1)
+#endif
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,12,0)
+#ifndef __GTK_DOC_IGNORE__
+#define SNDRV_HWDEP_IFACE_FW_MOTU       (SNDRV_HWDEP_IFACE_LINE6 + 1)
+#define SNDRV_HWDEP_IFACE_FW_FIREFACE   (SNDRV_HWDEP_IFACE_FW_MOTU + 1)
+#endif
+#endif
+
 /**
  *ALSAHwdepIfaceType:
  * @ALSAHWDEP_IFACE_TYPE_OPL2:              For OPL2 sound chip.
@@ -62,18 +75,9 @@ typedef enum {
     ALSAHWDEP_IFACE_TYPE_FW_OXFW        = SNDRV_HWDEP_IFACE_FW_OXFW,
     ALSAHWDEP_IFACE_TYPE_FW_DIGI00X     = SNDRV_HWDEP_IFACE_FW_DIGI00X,
     ALSAHWDEP_IFACE_TYPE_FW_TASCAM      = SNDRV_HWDEP_IFACE_FW_TASCAM,
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,9,0)
     ALSAHWDEP_IFACE_TYPE_LINE6          = SNDRV_HWDEP_IFACE_LINE6,
-#else
-    ALSAHWDEP_IFACE_TYPE_LINE6,
-#endif
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0)
     ALSAHWDEP_IFACE_TYPE_FW_MOTU        = SNDRV_HWDEP_IFACE_FW_MOTU,
     ALSAHWDEP_IFACE_TYPE_FW_FIREFACE    = SNDRV_HWDEP_IFACE_FW_FIREFACE,
-#else
-    ALSAHWDEP_IFACE_TYPE_FW_MOTU,
-    ALSAHWDEP_IFACE_TYPE_FW_FIREFACE,
-#endif
 } ALSAHwdepIfaceType;
 
 #endif