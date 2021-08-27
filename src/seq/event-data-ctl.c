alsa-project/alsa-gobject

commit a8f603eff4f5fbb9a22c14777be515a329563dbd

seq: event_data_ctl: add accessor methods
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-data-ctl.c
@@ -7,3 +7,79 @@ ALSASeqEventDataCtl *seq_event_data_ctl_copy(const ALSASeqEventDataCtl *self)
 }
 
 G_DEFINE_BOXED_TYPE(ALSASeqEventDataCtl, alsaseq_event_data_ctl, seq_event_data_ctl_copy, g_free)
+
+/**
+ * alsaseq_event_data_ctl_get_channel:
+ * @self: A #ALSASeqEventDataCtl.
+ *
+ * Get the value of channel for the control event.
+ *
+ * Returns: the value of channel for the control event.
+ */
+guint8 alsaseq_event_data_ctl_get_channel(ALSASeqEventDataCtl *self)
+{
+    return self->channel;
+}
+
+/**
+ * alsaseq_event_data_ctl_set_channel:
+ * @self: A #ALSASeqEventDataCtl.
+ * @channel: The channel for the control event.
+ *
+ * Set the channel for the control event.
+ */
+void alsaseq_event_data_ctl_set_channel(ALSASeqEventDataCtl *self,
+                                        guint8 channel)
+{
+    self->channel = channel;
+}
+
+/**
+ * alsaseq_event_data_ctl_get_param:
+ * @self: A #ALSASeqEventDataCtl.
+ *
+ * Get the parameter for the control event.
+ *
+ * Returns: the parameter for the control event.
+ */
+guint alsaseq_event_data_ctl_get_param(ALSASeqEventDataCtl *self)
+{
+    return self->param;
+}
+
+/**
+ * alsaseq_event_data_ctl_set_param:
+ * @self: A #ALSASeqEventDataCtl.
+ * @param: The parameter for the control event.
+ *
+ * Set the parameter for the control event.
+ */
+void alsaseq_event_data_ctl_set_param(ALSASeqEventDataCtl *self, guint param)
+{
+    self->param = param;
+}
+
+/**
+ * alsaseq_event_data_ctl_get_value:
+ * @self: A #ALSASeqEventDataCtl.
+ *
+ * Get the value for the control event.
+ *
+ * Returns: the value for the control event.
+ */
+gint alsaseq_event_data_ctl_get_value(ALSASeqEventDataCtl *self)
+{
+    return self->param;
+}
+
+/**
+ * alsaseq_event_data_ctl_set_value:
+ * @self: A #ALSASeqEventDataCtl.
+ * @value: The value for the control event.
+ *
+ * Set the value for the control event.
+ */
+void alsaseq_event_data_ctl_set_value(ALSASeqEventDataCtl *self, gint value)
+{
+    self->value = value;
+}

src/seq/alsaseq.map
@@ -90,6 +90,12 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_event_data_note_set_duration";
 
     "alsaseq_event_data_ctl_get_type";
+    "alsaseq_event_data_ctl_get_channel";
+    "alsaseq_event_data_ctl_set_channel";
+    "alsaseq_event_data_ctl_get_param";
+    "alsaseq_event_data_ctl_set_param";
+    "alsaseq_event_data_ctl_get_value";
+    "alsaseq_event_data_ctl_set_value";
   local:
     *;
 };

src/seq/event-data-ctl.h
@@ -17,6 +17,16 @@ typedef struct snd_seq_ev_ctrl ALSASeqEventDataCtl;
 
 GType alsaseq_event_data_ctl_get_type() G_GNUC_CONST;
 
+guint8 alsaseq_event_data_ctl_get_channel(ALSASeqEventDataCtl *self);
+void alsaseq_event_data_ctl_set_channel(ALSASeqEventDataCtl *self,
+                                        guint8 channel);
+
+guint alsaseq_event_data_ctl_get_param(ALSASeqEventDataCtl *self);
+void alsaseq_event_data_ctl_set_param(ALSASeqEventDataCtl *self, guint param);
+
+gint alsaseq_event_data_ctl_get_value(ALSASeqEventDataCtl *self);
+void alsaseq_event_data_ctl_set_value(ALSASeqEventDataCtl *self, gint value);
+
 G_END_DECLS
 
 #endif

commit c8f99212326c3fdbbc95e9760e56255545536764

seq: event_data_ctl: fix getter API for value parameter
ALSASeq.EventDataCtl.get_value() is programmed carelessly to return
the value in parameter field instead of value field.

This commit fixes the bug.

Fixes: a8f603eff4f5 ("seq: event_data_ctl: add accessor methods")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/event-data-ctl.c
@@ -79,7 +79,7 @@ void alsaseq_event_data_ctl_set_param(ALSASeqEventDataCtl *self, guint param)
 void alsaseq_event_data_ctl_get_value(const ALSASeqEventDataCtl *self,
                                       gint *value)
 {
-    *value = self->param;
+    *value = self->value;
 }
 
 /**