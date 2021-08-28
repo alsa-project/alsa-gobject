alsa-project/alsa-gobject

commit d1936fbbe7e296aa249b886428dcb45951342a1e

ctl: add global method to get list of available sound cards

src/ctl/query.c
@@ -2,5 +2,170 @@
 #include "query.h"
 #include "privates.h"
 
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+
+#include <libudev.h>
+
 // For error handling.
 G_DEFINE_QUARK("alsactl-error", alsactl_error)
+
+static void prepare_udev_enum(struct udev_enumerate **enumerator,
+                              GError **error)
+{
+    struct udev *ctx;
+    int err;
+
+    ctx = udev_new();
+    if (ctx == NULL) {
+        generate_error(error, errno);
+        return;
+    }
+
+    *enumerator = udev_enumerate_new(ctx);
+    if (*enumerator == NULL) {
+        generate_error(error, errno);
+        udev_unref(ctx);
+        return;
+    }
+
+    err = udev_enumerate_add_match_subsystem(*enumerator, "sound");
+    if (err < 0) {
+        generate_error(error, -err);
+        udev_enumerate_unref(*enumerator);
+        udev_unref(ctx);
+        return;
+    }
+
+    err = udev_enumerate_scan_devices(*enumerator);
+    if (err < 0) {
+        generate_error(error, -err);
+        udev_enumerate_unref(*enumerator);
+        udev_unref(ctx);
+    }
+}
+
+static struct udev_device *detect_dev(struct udev_enumerate *enumerator,
+                                      struct udev_list_entry *entry,
+                                      const char *prefix)
+{
+    struct udev *ctx = udev_enumerate_get_udev(enumerator);
+    const char *syspath;
+    struct udev_device *dev;
+    const char *sysname;
+
+    syspath = udev_list_entry_get_name(entry);
+    if (syspath == NULL)
+        return NULL;
+
+    dev = udev_device_new_from_syspath(ctx, syspath);
+    if (dev == NULL)
+        return NULL;
+
+    sysname = udev_device_get_sysname(dev);
+    if (sysname == NULL) {
+        udev_device_unref(dev);
+        return NULL;
+    }
+
+    if (strstr(sysname, prefix) != sysname) {
+        udev_device_unref(dev);
+        return NULL;
+    }
+
+    return dev;
+}
+
+static void release_udev_enum(struct udev_enumerate *enumerator)
+{
+    struct udev *ctx = udev_enumerate_get_udev(enumerator);
+
+    udev_enumerate_unref(enumerator);
+    udev_unref(ctx);
+}
+
+static int compare_guint(const void *l, const void *r)
+{
+    const guint *x = l;
+    const guint *y = r;
+
+    return *x > *y;
+}
+
+/**
+ * alsactl_get_card_id_list:
+ * @entries: (array length=entry_count)(out): The list of numerical ID for sound
+ *           cards.
+ * @entry_count: The number of entries.
+ * @error: A #GError.
+ *
+ * Get the list of numerical ID for available sound cards.
+ */
+void alsactl_get_card_id_list(guint **entries, gsize *entry_count,
+                              GError **error)
+{
+    struct udev_enumerate *enumerator;
+    struct udev_list_entry *entry, *entry_list;
+    unsigned int count;
+    unsigned int index;
+
+    if (entries == NULL || entry_count == NULL || error == NULL) {
+        generate_error(error, EINVAL);
+        return;
+    }
+
+    prepare_udev_enum(&enumerator, error);
+    if (enumerator == NULL)
+        return;
+
+    entry_list = udev_enumerate_get_list_entry(enumerator);
+
+    count = 0;
+    udev_list_entry_foreach(entry, entry_list) {
+        struct udev_device *dev = detect_dev(enumerator, entry, "card");
+        if (dev != NULL) {
+            ++count;
+            udev_device_unref(dev);
+        }
+    }
+
+    // Nothing available.
+    if (count == 0)
+        goto end;
+
+    *entries = g_try_malloc0_n(count, sizeof(**entries));
+    if (*entries == NULL) {
+        generate_error(error, ENOMEM);
+        goto end;
+    }
+
+    index = 0;
+    udev_list_entry_foreach(entry, entry_list) {
+        struct udev_device *dev = detect_dev(enumerator, entry, "card");
+        if (dev != NULL) {
+            const char *sysnum = udev_device_get_sysnum(dev);
+            long val;
+            char *endptr;
+
+            errno = 0;
+            val = strtol(sysnum, &endptr, 10);
+            if (errno == 0 && *endptr == '\0' && val >= 0) {
+                (*entries)[index] = (guint)val;
+                ++index;
+            }
+            udev_device_unref(dev);
+        }
+    }
+    if (index != count) {
+        generate_error(error, ENOENT);
+        g_free(*entries);
+        *entries = NULL;
+        goto end;
+    }
+    *entry_count = count;
+
+    qsort(*entries, count, sizeof(guint), compare_guint);
+end:
+    release_udev_enum(enumerator);
+}

src/ctl/alsactl.map
@@ -5,6 +5,8 @@ ALSA_GOBJECT_0_0_0 {
     "alsactl_elem_access_flag_get_type";
     "alsactl_event_type_get_type";
     "alsactl_event_mask_flag_get_type";
+
+    "alsactl_get_card_id_list";
   local:
     *;
 };

src/ctl/query.h
@@ -7,6 +7,9 @@
 
 G_BEGIN_DECLS
 
+void alsactl_get_card_id_list(guint **entries, gsize *entry_count,
+                              GError **error);
+
 G_END_DECLS
 
 #endif

commit a9c1f0258e221bd0721a99416984dc81cc27fdbd

ctl/rawmidi/hwdep: fix compiler warning due to uninitialized variables
In some libraries, the local variable of udev enumerate is carelessly
uninitialized. As a result, compiler generates warnings, like:

../src/ctl/query.c: In function ‘alsactl_get_card_id_list’:
../src/ctl/query.c:131:8: warning: ‘enumerator’ may be used uninitialized in this function [-Wmaybe-uninitialized]
  131 |     if (enumerator == NULL)
      |        ^

This commit fixes the bug.

Fixes: d1936fbbe7e2 ("ctl: add global method to get list of available sound cards")
Fixes: cb128c5b59e0 ("rawmidi; add global method to get list of rawmidi device for sound card")
Fixes: d4a9f6735d05 ("hwdep: add global method to get list of hwdep devices in sound card")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/ctl/query.c
@@ -118,7 +118,7 @@ static int compare_guint(const void *l, const void *r)
 void alsactl_get_card_id_list(guint **entries, gsize *entry_count,
                               GError **error)
 {
-    struct udev_enumerate *enumerator;
+    struct udev_enumerate *enumerator = NULL;
     struct udev_list_entry *entry, *entry_list;
     unsigned int count;
     unsigned int index;
@@ -128,7 +128,7 @@ void alsactl_get_card_id_list(guint **entries, gsize *entry_count,
     g_return_if_fail(error == NULL || *error == NULL);
 
     prepare_udev_enum(&enumerator, error);
-    if (enumerator == NULL)
+    if (*error == NULL)
         return;
 
     entry_list = udev_enumerate_get_list_entry(enumerator);

src/hwdep/query.c
@@ -141,7 +141,7 @@ static unsigned int calculate_digits(unsigned int number)
 void alsahwdep_get_device_id_list(guint card_id, guint **entries,
                                   gsize *entry_count, GError **error)
 {
-    struct udev_enumerate *enumerator;
+    struct udev_enumerate *enumerator = NULL;
     unsigned int length;
     char *prefix;
     struct udev_list_entry *entry, *entry_list;
@@ -154,7 +154,7 @@ void alsahwdep_get_device_id_list(guint card_id, guint **entries,
 
     prepare_udev_enum(&enumerator, error);
     if (*error != NULL)
-        goto end;
+        return;
 
     length = strlen(PREFIX_SYSNAME_TEMPLATE) + calculate_digits(card_id) + 1;
     prefix = g_malloc0(length);

src/rawmidi/query.c
@@ -141,7 +141,7 @@ static unsigned int calculate_digits(unsigned int number)
 void alsarawmidi_get_device_id_list(guint card_id, guint **entries,
                                     gsize *entry_count, GError **error)
 {
-    struct udev_enumerate *enumerator;
+    struct udev_enumerate *enumerator = NULL;
     unsigned int length;
     char *prefix;
     struct udev_list_entry *entry, *entry_list;
@@ -154,7 +154,7 @@ void alsarawmidi_get_device_id_list(guint card_id, guint **entries,
 
     prepare_udev_enum(&enumerator, error);
     if (*error != NULL)
-        goto end;
+        return;
 
     length = strlen(PREFIX_SYSNAME_TEMPLATE) + calculate_digits(card_id) + 1;
     prefix = g_malloc0(length);