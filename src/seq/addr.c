7e6b558274a414f42fe8bc9a697c7b7aaaf576c1
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-03-16 08:30:07 +0900

    ctl/timer/seq: optimization for GLib v2.68
    The usage of g_memdup() is deprecated since glib 2.68 because it has
    narrow integer conversion from gsize to guint.
    
    This commit replaces the usage with macro for g_memdup2().
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/addr.c
+++ src/seq/addr.c
@@ -13,7 +13,15 @@
  */
 ALSASeqAddr *seq_addr_copy(const ALSASeqAddr *self)
 {
-    return g_memdup(self, sizeof(*self));
+#ifdef g_memdup2
+    return g_memdup2(self, sizeof(*self));
+#else
+    // GLib v2.68 deprecated g_memdup() with concern about overflow by narrow conversion from size_t to
+    // unsigned int however it's safe in the local case.
+    gpointer ptr = g_malloc(sizeof(*self));
+    memcpy(ptr, self, sizeof(*self));
+    return ptr;
+#endif
 }
 
 G_DEFINE_BOXED_TYPE(ALSASeqAddr, alsaseq_addr, seq_addr_copy, g_free)
--- src/timer/event-data-tick.c
+++ src/timer/event-data-tick.c
@@ -15,7 +15,15 @@
  */
 ALSATimerEventDataTick *timer_event_data_tick_copy(const ALSATimerEventDataTick *self)
 {
-    return g_memdup(self, sizeof(*self));
+#ifdef g_memdup2
+    return g_memdup2(self, sizeof(*self));
+#else
+    // GLib v2.68 deprecated g_memdup() with concern about overflow by narrow conversion from size_t to
+    // unsigned int however it's safe in the local case.
+    gpointer ptr = g_malloc(sizeof(*self));
+    memcpy(ptr, self, sizeof(*self));
+    return ptr;
+#endif
 }
 
 G_DEFINE_BOXED_TYPE(ALSATimerEventDataTick, alsatimer_event_data_tick, timer_event_data_tick_copy, g_free)
