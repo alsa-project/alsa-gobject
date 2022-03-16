d09c0717a4e0881b9ffaadaef0729e25e93bda14
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-03-16 09:03:48 +0900

    seq: migrate to gi-docgen from gtk-doc
    This commit uses gi-docgen and obsolete gtk-doc.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

---
+++ doc/seq/urlmap.js
@@ -0,0 +1,5 @@
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'ALSATimer', 'https://alsa-project.github.io/alsa-gobject-docs/alsatimer/' ]
+]
