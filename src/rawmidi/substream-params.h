bc3f1527b38bee53aa37bef7ca3d6baa6f98bb1a
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    rawmidi; substream_params: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/rawmidi/substream-params.h
+++ src/rawmidi/substream-params.h
@@ -8,41 +8,11 @@ G_BEGIN_DECLS
 
 #define ALSARAWMIDI_TYPE_SUBSTREAM_PARAMS (alsarawmidi_substream_params_get_type())
 
-#define ALSARAWMIDI_SUBSTREAM_PARAMS(obj)                           \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                              \
-                                ALSARAWMIDI_TYPE_SUBSTREAM_PARAMS,  \
-                                ALSARawmidiSubstreamParams))
-#define ALSARAWMIDI_IS_SUBSTREAM_PARAMS(obj)                        \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                              \
-                                ALSARAWMIDI_TYPE_SUBSTREAM_PARAMS))
-
-#define ALSARAWMIDI_SUBSTREAM_PARAMS_CLASS(klass)                   \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                               \
-                             ALSARAWMIDI_TYPE_SUBSTREAM_PARAMS,     \
-                             ALSARawmidiSubstreamParamsClass))
-#define ALSARAWMIDI_IS_SUBSTREAM_PARAMS_CLASS(klass)                \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                               \
-                             ALSARAWMIDI_TYPE_SUBSTREAM_PARAMS))
-#define ALSARAWMIDI_SUBSTREAM_PARAMS_GET_CLASS(obj)                 \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                               \
-                               ALSARAWMIDI_TYPE_SUBSTREAM_PARAMS,   \
-                               ALSARawmidiSubstreamParamsClass))
-
-typedef struct _ALSARawmidiSubstreamParams          ALSARawmidiSubstreamParams;
-typedef struct _ALSARawmidiSubstreamParamsClass     ALSARawmidiSubstreamParamsClass;
-typedef struct _ALSARawmidiSubstreamParamsPrivate   ALSARawmidiSubstreamParamsPrivate;
-
-struct _ALSARawmidiSubstreamParams {
-    GObject parent_instance;
-
-    ALSARawmidiSubstreamParamsPrivate *priv;
-};
-
+G_DECLARE_DERIVABLE_TYPE(ALSARawmidiSubstreamParams, alsarawmidi_substream_params, ALSARAWMIDI,
+                         SUBSTREAM_PARAMS, GObject);
 struct _ALSARawmidiSubstreamParamsClass {
     GObjectClass parent_class;
 };
-
-GType alsarawmidi_substream_params_get_type(void) G_GNUC_CONST;
 
 ALSARawmidiSubstreamParams *alsarawmidi_substream_params_new();
 
