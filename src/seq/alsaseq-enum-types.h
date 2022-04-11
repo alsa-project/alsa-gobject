alsa-project/alsa-gobject

commit 9a3d1477403b5c352ce292d4789f121a5601a881

seq: user_client: report error for port access permission
Port in ALSA sequencer has capability and client has access permission
to operate the port. When unpermitted, the operation fails and return
EPERM error code.

This commit handles the code for local error.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/alsaseq-enum-types.h
@@ -364,12 +364,14 @@ typedef enum /*< flags >*/
 
 /**
  * ALSASeqUserClientError:
- * @ALSASEQ_USER_CLIENT_ERROR_FAILED:	The system call failed.
+ * @ALSASEQ_USER_CLIENT_ERROR_FAILED:	            The system call failed.
+ * @ALSASEQ_USER_CLIENT_ERROR_PORT_PERMISSION:      The operation fails due to access permission of port.
  *
  * A set of error code for GError with domain which equals to #alsaseq_user_client_error_quark()
  */
 typedef enum {
     ALSASEQ_USER_CLIENT_ERROR_FAILED,
+    ALSASEQ_USER_CLIENT_ERROR_PORT_PERMISSION,
 } ALSASeqUserClientError;
 
 #endif

src/seq/user-client.c
@@ -43,6 +43,13 @@ G_DEFINE_TYPE_WITH_PRIVATE(ALSASeqUserClient, alsaseq_user_client, G_TYPE_OBJECT
  */
 G_DEFINE_QUARK(alsaseq-user-client-error-quark, alsaseq_user_client_error)
 
+static const char *const err_msgs[] = {
+        [ALSASEQ_USER_CLIENT_ERROR_PORT_PERMISSION] = "The operation fails due to access permission of port",
+};
+
+#define generate_local_error(exception, code) \
+        g_set_error_literal(exception, ALSASEQ_USER_CLIENT_ERROR, code, err_msgs[code])
+
 #define generate_syscall_error(exception, errno, fmt, arg) \
         g_set_error(exception, ALSASEQ_USER_CLIENT_ERROR, ALSASEQ_USER_CLIENT_ERROR_FAILED, \
                     fmt" %d(%s)", arg, errno, strerror(errno))
@@ -680,8 +687,12 @@ void alsaseq_user_client_operate_subscription(ALSASeqUserClient *self,
         label = "UNSUBSCRIBE_PORT";
     }
 
-    if (ioctl(priv->fd, request, data) < 0)
-        generate_syscall_error(error, errno, "ioctl(%s)", label);
+    if (ioctl(priv->fd, request, data) < 0) {
+        if (errno == EPERM)
+            generate_local_error(error, ALSASEQ_USER_CLIENT_ERROR_PORT_PERMISSION);
+        else
+            generate_syscall_error(error, errno, "ioctl(%s)", label);
+    }
 }
 
 /**

tests/alsaseq-enums
@@ -166,6 +166,7 @@ remove_filter_flags = (
 
 user_client_error_types = (
     'FAILED',
+    'PORT_PERMISSION',
 )
 
 types = {

commit ba981c1e05516f349d9823b5cf9bb281d842ddc2

seq: enum: fix tab indentation
Fixes: 9a3d1477403b ("seq: user_client: report error for port access permission")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/alsaseq-enum-types.h
@@ -362,7 +362,7 @@ typedef enum /*< flags >*/
 
 /**
  * ALSASeqUserClientError:
- * @ALSASEQ_USER_CLIENT_ERROR_FAILED:	            The system call failed.
+ * @ALSASEQ_USER_CLIENT_ERROR_FAILED:               The system call failed.
  * @ALSASEQ_USER_CLIENT_ERROR_PORT_PERMISSION:      The operation fails due to access permission of port.
  * @ALSASEQ_USER_CLIENT_ERROR_QUEUE_PERMISSION:     The operation fails due to access permission of queue.
  *