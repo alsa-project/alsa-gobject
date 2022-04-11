acf29a92540f76c6a00e9755762320e681899613
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    timer: device_info: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/timer/device-info.h
+++ src/timer/device-info.h
@@ -8,41 +8,12 @@ G_BEGIN_DECLS
 
 #define ALSATIMER_TYPE_DEVICE_INFO   (alsatimer_device_info_get_type())
 
-#define ALSATIMER_DEVICE_INFO(obj)                              \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                          \
-                                ALSATIMER_TYPE_DEVICE_INFO,     \
-                                ALSATimerDeviceInfo))
-#define ALSATIMER_IS_DEVICE_INFO(obj)                           \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                          \
-                                ALSATIMER_TYPE_DEVICE_INFO))
-
-#define ALSATIMER_DEVICE_INFO_CLASS(klass)                      \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                           \
-                             ALSATIMER_TYPE_DEVICE_INFO,        \
-                             ALSATimerDeviceInfoClass))
-#define ALSATIMER_IS_DEVICE_INFO_CLASS(klass)                   \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                           \
-                             ALSATIMER_TYPE_DEVICE_INFO))
-#define ALSATIMER_DEVICE_INFO_GET_CLASS(obj)                    \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                           \
-                               ALSATIMER_TYPE_DEVICE_INFO,      \
-                               ALSATimerDeviceInfoClass))
-
-typedef struct _ALSATimerDeviceInfo         ALSATimerDeviceInfo;
-typedef struct _ALSATimerDeviceInfoClass    ALSATimerDeviceInfoClass;
-typedef struct _ALSATimerDeviceInfoPrivate  ALSATimerDeviceInfoPrivate;
-
-struct _ALSATimerDeviceInfo {
-    GObject parent_instance;
-
-    ALSATimerDeviceInfoPrivate *priv;
-};
+G_DECLARE_DERIVABLE_TYPE(ALSATimerDeviceInfo, alsatimer_device_info, ALSATIMER, DEVICE_INFO,
+                         GObject);
 
 struct _ALSATimerDeviceInfoClass {
     GObjectClass parent_class;
 };
-
-GType alsatimer_device_info_get_type() G_GNUC_CONST;
 
 G_END_DECLS
 
