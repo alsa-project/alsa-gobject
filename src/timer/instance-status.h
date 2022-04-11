5b50489b2514191a5c8066f9a7ed8774c1608a8d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    timer: instance_status: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/timer/instance-status.h
+++ src/timer/instance-status.h
@@ -8,41 +8,12 @@ G_BEGIN_DECLS
 
 #define ALSATIMER_TYPE_INSTANCE_STATUS  (alsatimer_instance_status_get_type())
 
-#define ALSATIMER_INSTANCE_STATUS(obj)                          \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                          \
-                                ALSATIMER_TYPE_INSTANCE_STATUS, \
-                                ALSATimerInstanceStatus))
-#define ALSATIMER_IS_INSTANCE_STATUS(obj)                       \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                          \
-                                ALSATIMER_TYPE_INSTANCE_STATUS))
-
-#define ALSATIMER_INSTANCE_STATUS_CLASS(klass)                  \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                           \
-                             ALSATIMER_TYPE_INSTANCE_STATUS,    \
-                             ALSATimerInstanceStatusClass))
-#define ALSATIMER_IS_INSTANCE_STATUS_CLASS(klass)               \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                           \
-                             ALSATIMER_TYPE_INSTANCE_STATUS))
-#define ALSATIMER_INSTANCE_STATUS_GET_CLASS(obj)                \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                           \
-                               ALSATIMER_TYPE_INSTANCE_STATUS,  \
-                               ALSATimerInstanceStatusClass))
-
-typedef struct _ALSATimerInstanceStatus         ALSATimerInstanceStatus;
-typedef struct _ALSATimerInstanceStatusClass    ALSATimerInstanceStatusClass;
-typedef struct _ALSATimerInstanceStatusPrivate  ALSATimerInstanceStatusPrivate;
-
-struct _ALSATimerInstanceStatus {
-    GObject parent_instance;
-
-    ALSATimerInstanceStatusPrivate *priv;
-};
+G_DECLARE_DERIVABLE_TYPE(ALSATimerInstanceStatus, alsatimer_instance_status, ALSATIMER,
+                         INSTANCE_STATUS, GObject);
 
 struct _ALSATimerInstanceStatusClass {
     GObjectClass parent_class;
 };
-
-GType alsatimer_instance_status_get_type() G_GNUC_CONST;
 
 ALSATimerInstanceStatus *alsatimer_instance_status_new();
 
