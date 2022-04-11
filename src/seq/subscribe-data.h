fd3b3c8c3d09e9dcf842d5b302185f114904182b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    seq: subscribe_data: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/subscribe-data.h
+++ src/seq/subscribe-data.h
@@ -8,41 +8,11 @@ G_BEGIN_DECLS
 
 #define ALSASEQ_TYPE_SUBSCRIBE_DATA     (alsaseq_subscribe_data_get_type())
 
-#define ALSASEQ_SUBSCRIBE_DATA(obj)                             \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                          \
-                                ALSASEQ_TYPE_SUBSCRIBE_DATA,    \
-                                ALSASeqSubscribeData))
-#define ALSASEQ_IS_SUBSCRIBE_DATA(obj)                          \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                          \
-                                ALSASEQ_TYPE_SUBSCRIBE_DATA))
-
-#define ALSASEQ_SUBSCRIBE_DATA_CLASS(klass)                     \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                           \
-                             ALSASEQ_TYPE_SUBSCRIBE_DATA,       \
-                             ALSASeqSubscribeDataClass))
-#define ALSASEQ_IS_SUBSCRIBE_DATA_CLASS(klass)                  \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                           \
-                             ALSASEQ_TYPE_SUBSCRIBE_DATA))
-#define ALSASEQ_SUBSCRIBE_DATA_GET_CLASS(obj)                   \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                           \
-                               ALSASEQ_TYPE_SUBSCRIBE_DATA,     \
-                               ALSASeqSubscribeDataClass))
-
-typedef struct _ALSASeqSubscribeData            ALSASeqSubscribeData;
-typedef struct _ALSASeqSubscribeDataClass       ALSASeqSubscribeDataClass;
-typedef struct _ALSASeqSubscribeDataPrivate     ALSASeqSubscribeDataPrivate;
-
-struct _ALSASeqSubscribeData {
-    GObject parent_instance;
-
-    ALSASeqSubscribeData *priv;
-};
+G_DECLARE_DERIVABLE_TYPE(ALSASeqSubscribeData, alsaseq_subscribe_data, ALSASEQ, SUBSCRIBE_DATA, GObject);
 
 struct _ALSASeqSubscribeDataClass {
     GObjectClass parent_class;
 };
-
-GType alsaseq_subscribe_data_get_type() G_GNUC_CONST;
 
 ALSASeqSubscribeData *alsaseq_subscribe_data_new();
 
