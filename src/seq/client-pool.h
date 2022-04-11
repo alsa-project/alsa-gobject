45c957a4c292a7a638d2957af9c7ab5720921060
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    seq: client_pool: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/client-pool.h
+++ src/seq/client-pool.h
@@ -8,41 +8,11 @@ G_BEGIN_DECLS
 
 #define ALSASEQ_TYPE_CLIENT_POOL        (alsaseq_client_pool_get_type())
 
-#define ALSASEQ_CLIENT_POOL(obj)                            \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                      \
-                                ALSASEQ_TYPE_CLIENT_POOL,   \
-                                ALSASeqClientPool))
-#define ALSASEQ_IS_CLIENT_POOL(obj)                         \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                      \
-                                ALSASEQ_TYPE_CLIENT_POOL))
-
-#define ALSASEQ_CLIENT_POOL_CLASS(klass)                    \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                       \
-                             ALSASEQ_TYPE_CLIENT_POOL,      \
-                             ALSASeqClientPoolClass))
-#define ALSASEQ_IS_CLIENT_POOL_CLASS(klass)                 \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                       \
-                             ALSASEQ_TYPE_CLIENT_POOL))
-#define ALSASEQ_CLIENT_POOL_GET_CLASS(obj)                  \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                       \
-                               ALSASEQ_TYPE_CLIENT_POOL,    \
-                               ALSASeqClientPoolClass))
-
-typedef struct _ALSASeqClientPool           ALSASeqClientPool;
-typedef struct _ALSASeqClientPoolClass      ALSASeqClientPoolClass;
-typedef struct _ALSASeqClientPoolPrivate    ALSASeqClientPoolPrivate;
-
-struct _ALSASeqClientPool {
-    GObject parent_instance;
-
-    ALSASeqClientPoolPrivate *priv;
-};
+G_DECLARE_DERIVABLE_TYPE(ALSASeqClientPool, alsaseq_client_pool, ALSASEQ, CLIENT_POOL, GObject);
 
 struct _ALSASeqClientPoolClass {
     GObjectClass parent_class;
 };
-
-GType alsaseq_client_pool_get_type() G_GNUC_CONST;
 
 ALSASeqClientPool *alsaseq_client_pool_new();
 
