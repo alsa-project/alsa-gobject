cf720c7264a82d639ec70fd21173783f1d1aabcf
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    ctl: card: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/ctl/card.h
+++ src/ctl/card.h
@@ -8,39 +8,11 @@ G_BEGIN_DECLS
 
 #define ALSACTL_TYPE_CARD   (alsactl_card_get_type())
 
-#define ALSACTL_CARD(obj)                           \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),              \
-                                ALSACTL_TYPE_CARD,  \
-                                ALSACtlCard))
-#define ALSACTL_IS_CARD(obj)                        \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),              \
-                                ALSACTL_TYPE_CARD))
+G_DECLARE_DERIVABLE_TYPE(ALSACtlCard, alsactl_card, ALSACTL, CARD, GObject);
 
-#define ALSACTL_CARD_CLASS(klass)                   \
-    (G_TYPE_CHECK_CLASS_CAST((klass),               \
-                             ALSACTL_TYPE_CARD,     \
-                             ALSACtlCardClass))
-#define ALSACTL_IS_CARD_CLASS(klass)                \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),               \
-                             ALSACTL_TYPE_CARD))
-#define ALSACTL_CARD_GET_CLASS(obj)                 \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),               \
-                               ALSACTL_TYPE_CARD,   \
-                               ALSACtlCardClass))
-
-#define ALSACTL_CARD_ERROR	alsactl_card_error_quark()
+#define ALSACTL_CARD_ERROR  alsactl_card_error_quark()
 
 GQuark alsactl_card_error_quark();
-
-typedef struct _ALSACtlCard         ALSACtlCard;
-typedef struct _ALSACtlCardClass    ALSACtlCardClass;
-typedef struct _ALSACtlCardPrivate  ALSACtlCardPrivate;
-
-struct _ALSACtlCard {
-    GObject parent_instance;
-
-    ALSACtlCardPrivate *priv;
-};
 
 struct _ALSACtlCardClass {
     GObjectClass parent_class;
@@ -67,8 +39,6 @@ struct _ALSACtlCardClass {
      */
     void (*handle_disconnection)(ALSACtlCard *self);
 };
-
-GType alsactl_card_get_type() G_GNUC_CONST;
 
 ALSACtlCard *alsactl_card_new();
 
