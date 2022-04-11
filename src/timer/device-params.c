alsa-project/alsa-gobject

commit 20cb2a1f111009aa184d32ab221e032188f4cf5e

timer: device_params: add properties
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/timer/device-params.c
@@ -1,14 +1,96 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-#include "device-params.h"
+#include "privates.h"
 
+struct _ALSATimerDeviceParamsPrivate {
+    struct snd_timer_gparams params;
+};
 G_DEFINE_TYPE(ALSATimerDeviceParams, alsatimer_device_params, G_TYPE_OBJECT)
+
+enum timer_device_params_prop_type {
+    TIMER_DEVICE_PARAMS_PROP_PERIOD_NUM = 1,
+    TIMER_DEVICE_PARAMS_PROP_PERIOD_DEN,
+    TIMER_DEVICE_PARAMS_PROP_COUNT,
+};
+static GParamSpec *timer_device_params_props[TIMER_DEVICE_PARAMS_PROP_COUNT] = { NULL, };
+
+static void timer_device_params_set_property(GObject *obj, guint id,
+                                            const GValue *val, GParamSpec *spec)
+{
+    ALSATimerDeviceParams *self = ALSATIMER_DEVICE_PARAMS(obj);
+    ALSATimerDeviceParamsPrivate *priv =
+                            alsatimer_device_params_get_instance_private(self);
+
+    switch (id) {
+    case TIMER_DEVICE_PARAMS_PROP_PERIOD_NUM:
+        priv->params.period_num = (unsigned long)g_value_get_uint64(val);
+        break;
+    case TIMER_DEVICE_PARAMS_PROP_PERIOD_DEN:
+        priv->params.period_den = (unsigned long)g_value_get_uint64(val);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, id, spec);
+        break;
+    }
+}
+
+static void timer_device_params_get_property(GObject *obj, guint id,
+                                             GValue *val, GParamSpec *spec)
+{
+    ALSATimerDeviceParams *self = ALSATIMER_DEVICE_PARAMS(obj);
+    ALSATimerDeviceParamsPrivate *priv =
+                            alsatimer_device_params_get_instance_private(self);
+
+    switch (id) {
+    case TIMER_DEVICE_PARAMS_PROP_PERIOD_NUM:
+        g_value_set_uint64(val, priv->params.period_num);
+        break;
+    case TIMER_DEVICE_PARAMS_PROP_PERIOD_DEN:
+        g_value_set_uint64(val, priv->params.period_den);
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, id, spec);
+        break;
+    }
+}
 
 static void alsatimer_device_params_class_init(ALSATimerDeviceParamsClass *klass)
 {
-    return;
+    GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
+
+    gobject_class->set_property = timer_device_params_set_property;
+    gobject_class->get_property = timer_device_params_get_property;
+
+    timer_device_params_props[TIMER_DEVICE_PARAMS_PROP_PERIOD_NUM] =
+        g_param_spec_uint64("period-numerator", "period-numerator",
+                            "The numerator of period for timer.",
+                            0, G_MAXUINT64,
+                            0,
+                            G_PARAM_READWRITE);
+
+    timer_device_params_props[TIMER_DEVICE_PARAMS_PROP_PERIOD_DEN] =
+        g_param_spec_uint64("period-denominator", "period-denominator",
+                            "The denominator of period for timer.",
+                            0, G_MAXUINT64,
+                            0,
+                            G_PARAM_READWRITE);
+
+    g_object_class_install_properties(gobject_class,
+                    TIMER_DEVICE_PARAMS_PROP_COUNT, timer_device_params_props);
 }
 
 static void alsatimer_device_params_init(ALSATimerDeviceParams *self)
 {
     return;
+}
+
+/**
+ * alsatimer_device_params_new:
+ *
+ * Instantiate #ALSATimerDeviceParams object and return the instance.
+ *
+ * Returns: an instance of #ALSATimerDeviceParams.
+ */
+ALSATimerDeviceParams *alsatimer_device_params_new()
+{
+    return g_object_new(ALSATIMER_TYPE_DEVICE_PARAMS, NULL);
 }

src/timer/alsatimer.map
@@ -25,6 +25,7 @@ ALSA_GOBJECT_0_0_0 {
     "alsatimer_device_status_get_type";
 
     "alsatimer_device_params_get_type";
+    "alsatimer_device_params_new";
   local:
     *;
 };

src/timer/device-params.h
@@ -5,6 +5,8 @@
 #include <glib.h>
 #include <glib-object.h>
 
+#include <timer/device-id.h>
+
 G_BEGIN_DECLS
 
 #define ALSATIMER_TYPE_DEVICE_PARAMS    (alsatimer_device_params_get_type())
@@ -31,9 +33,12 @@ G_BEGIN_DECLS
 
 typedef struct _ALSATimerDeviceParams           ALSATimerDeviceParams;
 typedef struct _ALSATimerDeviceParamsClass      ALSATimerDeviceParamsClass;
+typedef struct _ALSATimerDeviceParamsPrivate    ALSATimerDeviceParamsPrivate;
 
 struct _ALSATimerDeviceParams {
     GObject parent_instance;
+
+    ALSATimerDeviceParamsPrivate *priv;
 };
 
 struct _ALSATimerDeviceParamsClass {
@@ -41,6 +46,8 @@ struct _ALSATimerDeviceParamsClass {
 };
 
 GType alsatimer_device_params_get_type() G_GNUC_CONST;
+
+ALSATimerDeviceParams *alsatimer_device_params_new();
 
 G_END_DECLS
 

src/timer/privates.h
@@ -4,6 +4,7 @@
 
 #include "device-info.h"
 #include "device-status.h"
+#include "device-params.h"
 
 #include <sound/asound.h>
 

tests/alsatimer-device-params
@@ -10,8 +10,13 @@ gi.require_version('ALSATimer', '0.0')
 from gi.repository import ALSATimer
 
 target = ALSATimer.DeviceParams()
-props = ()
-methods = ()
+props = (
+    'period-numerator',
+    'period-denominator',
+)
+methods = (
+    'new',
+)
 signals = ()
 
 if not test(target, props, methods, signals):

commit 81d726cecb88325b9eba8667f019ffb993701f65

timer: device_params: fix type decration with private data
Fixes: 20cb2a1f111 ("timer: device_params: add properties")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/timer/device-params.c
@@ -16,7 +16,7 @@
 struct _ALSATimerDeviceParamsPrivate {
     struct snd_timer_gparams params;
 };
-G_DEFINE_TYPE(ALSATimerDeviceParams, alsatimer_device_params, G_TYPE_OBJECT)
+G_DEFINE_TYPE_WITH_PRIVATE(ALSATimerDeviceParams, alsatimer_device_params, G_TYPE_OBJECT)
 
 enum timer_device_params_prop_type {
     TIMER_DEVICE_PARAMS_PROP_PERIOD_NUM = 1,