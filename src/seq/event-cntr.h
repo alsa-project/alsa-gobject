3614e3b87a73b4549d465d4b8da0276966561bf2
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    seq: event_cntr: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/event-cntr.h
+++ src/seq/event-cntr.h
@@ -8,41 +8,11 @@ G_BEGIN_DECLS
 
 #define ALSASEQ_TYPE_EVENT_CNTR     (alsaseq_event_cntr_get_type())
 
-#define ALSASEQ_EVENT_CNTR(obj)                             \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                      \
-                                ALSASEQ_TYPE_EVENT_CNTR,    \
-                                ALSASeqEventCntr))
-#define ALSASEQ_IS_EVENT_CNTR(obj)                          \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                      \
-                                ALSASEQ_TYPE_EVENT_CNTR))
-
-#define ALSASEQ_EVENT_CNTR_CLASS(klass)                     \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                       \
-                             ALSASEQ_TYPE_EVENT_CNTR,       \
-                             ALSASeqEventCntrClass))
-#define ALSASEQ_IS_EVENT_CNTR_CLASS(klass)                  \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                       \
-                             ALSASEQ_TYPE_EVENT_CNTR))
-#define ALSASEQ_EVENT_CNTR_GET_CLASS(obj)                   \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                       \
-                               ALSASEQ_TYPE_EVENT_CNTR,     \
-                               ALSASeqEventCntrClass))
-
-typedef struct _ALSASeqEventCntr            ALSASeqEventCntr;
-typedef struct _ALSASeqEventCntrClass       ALSASeqEventCntrClass;
-typedef struct _ALSASeqEventCntrPrivate     ALSASeqEventCntrPrivate;
-
-struct _ALSASeqEventCntr {
-    GObject parent_instance;
-
-    ALSASeqEventCntrPrivate *priv;
-};
+G_DECLARE_DERIVABLE_TYPE(ALSASeqEventCntr, alsaseq_event_cntr, ALSASEQ, EVENT_CNTR, GObject);
 
 struct _ALSASeqEventCntrClass {
     GObjectClass parent_class;
 };
-
-GType alsaseq_event_cntr_get_type() G_GNUC_CONST;
 
 ALSASeqEventCntr *alsaseq_event_cntr_new(guint count, GError **error);
 
