1dcf621d3e3a22169638bd654da5a8e8cd336e8d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    seq: system_info: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/system-info.h
+++ src/seq/system-info.h
@@ -8,41 +8,11 @@ G_BEGIN_DECLS
 
 #define ALSASEQ_TYPE_SYSTEM_INFO     (alsaseq_system_info_get_type())
 
-#define ALSASEQ_SYSTEM_INFO(obj)                            \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                      \
-                                ALSASEQ_TYPE_SYSTEM_INFO,   \
-                                ALSASeqSystemInfo))
-#define ALSASEQ_IS_SYSTEM_INFO(obj)                         \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                      \
-                                ALSASEQ_TYPE_SYSTEM_INFO))
-
-#define ALSASEQ_SYSTEM_INFO_CLASS(klass)                    \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                       \
-                             ALSASEQ_TYPE_SYSTEM_INFO,      \
-                             ALSASeqSystemInfoClass))
-#define ALSASEQ_IS_SYSTEM_INFO_CLASS(klass)                 \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                       \
-                             ALSASEQ_TYPE_SYSTEM_INFO))
-#define ALSASEQ_SYSTEM_INFO_GET_CLASS(obj)                  \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                       \
-                               ALSASEQ_TYPE_SYSTEM_INFO,    \
-                               ALSASeqSystemInfoClass))
-
-typedef struct _ALSASeqSystemInfo           ALSASeqSystemInfo;
-typedef struct _ALSASeqSystemInfoClass      ALSASeqSystemInfoClass;
-typedef struct _ALSASeqSystemInfoPrivate    ALSASeqSystemInfoPrivate;
-
-struct _ALSASeqSystemInfo {
-    GObject parent_instance;
-
-    ALSASeqSystemInfoPrivate *priv;
-};
+G_DECLARE_DERIVABLE_TYPE(ALSASeqSystemInfo, alsaseq_system_info, ALSASEQ, SYSTEM_INFO, GObject);
 
 struct _ALSASeqSystemInfoClass {
     GObjectClass parent_class;
 };
-
-GType alsaseq_system_info_get_type() G_GNUC_CONST;
 
 G_END_DECLS
 
