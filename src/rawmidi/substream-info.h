7fa922934ef4f10430092d581c6fdc6d3e332f22
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:42:26 +0900

    rawmidi: substream_info: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/rawmidi/substream-info.h
+++ src/rawmidi/substream-info.h
@@ -8,41 +8,11 @@ G_BEGIN_DECLS
 
 #define ALSARAWMIDI_TYPE_SUBSTREAM_INFO (alsarawmidi_substream_info_get_type())
 
-#define ALSARAWMIDI_SUBSTREAM_INFO(obj)                             \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj),                              \
-                                ALSARAWMIDI_TYPE_SUBSTREAM_INFO,    \
-                                ALSARawmidiSubstreamInfo))
-#define ALSARAWMIDI_IS_SUBSTREAM_INFO(obj)                          \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                              \
-                                ALSARAWMIDI_TYPE_SUBSTREAM_INFO))
-
-#define ALSARAWMIDI_SUBSTREAM_INFO_CLASS(klass)                     \
-    (G_TYPE_CHECK_CLASS_CAST((klass),                               \
-                             ALSARAWMIDI_TYPE_SUBSTREAM_INFO,       \
-                             ALSARawmidiSubstreamInfoClass))
-#define ALSARAWMIDI_IS_SUBSTREAM_INFO_CLASS(klass)                  \
-    (G_TYPE_CHECK_CLASS_TYPE((klass),                               \
-                             ALSARAWMIDI_TYPE_SUBSTREAM_INFO))
-#define ALSARAWMIDI_SUBSTREAM_INFO_GET_CLASS(obj)                   \
-    (G_TYPE_INSTANCE_GET_CLASS((obj),                               \
-                               ALSARAWMIDI_TYPE_SUBSTREAM_INFO,     \
-                               ALSARawmidiSubstreamInfoClass))
-
-typedef struct _ALSARawmidiSubstreamInfo        ALSARawmidiSubstreamInfo;
-typedef struct _ALSARawmidiSubstreamInfoClass   ALSARawmidiSubstreamInfoClass;
-typedef struct _ALSARawmidiSubstreamInfoPrivate ALSARawmidiSubstreamInfoPrivate;
-
-struct _ALSARawmidiSubstreamInfo {
-    GObject parent_instance;
-
-    ALSARawmidiSubstreamInfoPrivate *priv;
-};
-
+G_DECLARE_DERIVABLE_TYPE(ALSARawmidiSubstreamInfo, alsarawmidi_substream_info, ALSARAWMIDI,
+                         SUBSTREAM_INFO, GObject);
 struct _ALSARawmidiSubstreamInfoClass {
     GObjectClass parent_class;
 };
-
-GType alsarawmidi_substream_info_get_type() G_GNUC_CONST;
 
 G_END_DECLS
 
