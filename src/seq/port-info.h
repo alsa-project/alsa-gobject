alsa-project/alsa-gobject

commit 2ce823529df88007aa81ca1797c113df16313aa4

seq: port_info: add object to represent information of port for ALSA Sequencer
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/port-info.h
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#ifndef __ALSA_GOBJECT_ALSASEQ_PORT_INFO__H__
+#define __ALSA_GOBJECT_ALSASEQ_PORT_INFO__H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define ALSASEQ_TYPE_PORT_INFO      (alsaseq_port_info_get_type())
+
+#define ALSASEQ_PORT_INFO(obj)                              \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj),                      \
+                                ALSASEQ_TYPE_PORT_INFO,     \
+                                ALSASeqPortInfo))
+#define ALSASEQ_IS_PORT_INFO(obj)                           \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj),                      \
+                                ALSASEQ_TYPE_PORT_INFO))
+
+#define ALSASEQ_PORT_INFO_CLASS(klass)                      \
+    (G_TYPE_CHECK_CLASS_CAST((klass),                       \
+                             ALSASEQ_TYPE_PORT_INFO,        \
+                             ALSASeqPortInfoClass))
+#define ALSASEQ_IS_PORT_INFO_CLASS(klass)                   \
+    (G_TYPE_CHECK_CLASS_TYPE((klass),                       \
+                             ALSASEQ_TYPE_PORT_INFO))
+#define ALSASEQ_PORT_INFO_GET_CLASS(obj)                    \
+    (G_TYPE_INSTANCE_GET_CLASS((obj),                       \
+                               ALSASEQ_TYPE_PORT_INFO,      \
+                               ALSASeqPortInfoClass))
+
+typedef struct _ALSASeqPortInfo         ALSASeqPortInfo;
+typedef struct _ALSASeqPortInfoClass    ALSASeqPortInfoClass;
+
+struct _ALSASeqPortInfo {
+    GObject parent_instance;
+};
+
+struct _ALSASeqPortInfoClass {
+    GObjectClass parent_class;
+};
+
+GType alsaseq_port_info_get_type() G_GNUC_CONST;
+
+ALSASeqPortInfo *alsaseq_port_info_new();
+
+#endif

src/seq/alsaseq.map
@@ -40,6 +40,9 @@ ALSA_GOBJECT_0_0_0 {
     "alsaseq_addr_new";
     "alsaseq_addr_get_client_id";
     "alsaseq_addr_get_port_id";
+
+    "alsaseq_port_info_get_type";
+    "alsaseq_port_info_new";
   local:
     *;
 };

src/seq/meson.build
@@ -14,6 +14,7 @@ sources = files(
   'client-info.c',
   'user-client.c',
   'addr.c',
+  'port-info.c',
 )
 
 headers = files(
@@ -22,6 +23,7 @@ headers = files(
   'client-info.h',
   'user-client.h',
   'addr.h',
+  'port-info.h',
 )
 
 privates = files(

src/seq/port-info.c
@@ -1,0 +1,24 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "port-info.h"
+
+G_DEFINE_TYPE(ALSASeqPortInfo, alsaseq_port_info, G_TYPE_OBJECT)
+
+static void alsaseq_port_info_class_init(ALSASeqPortInfoClass *klass)
+{
+    return;
+}
+
+static void alsaseq_port_info_init(ALSASeqPortInfo *self)
+{
+    return;
+}
+
+/**
+ * alsaseq_port_info_new:
+ *
+ * Allocate and return an instance of ALSASeqPortInfo class.
+ */
+ALSASeqPortInfo *alsaseq_port_info_new()
+{
+    return g_object_new(ALSASEQ_TYPE_PORT_INFO, NULL);
+}

tests/alsaseq-port-info
@@ -1,0 +1,20 @@
+#!/usr/bin/env python3
+
+from sys import exit
+from errno import ENXIO
+
+from helper import test
+
+import gi
+gi.require_version('ALSASeq', '0.0')
+from gi.repository import ALSASeq
+
+target = ALSASeq.PortInfo()
+props = ()
+methods = (
+    'new',
+)
+signals = ()
+
+if not test(target, props, methods, signals):
+    exit(ENXIO)

tests/meson.build
@@ -33,6 +33,7 @@ tests = {
     'alsaseq-system-info',
     'alsaseq-client-info',
     'alsaseq-user-client',
+    'alsaseq-port-info',
   ],
 }
 

commit fd7f1cd74463a48db779f0b7abfcbae312e0183c

seq: port-info: fix pair of declaration macro
Fixes: 2ce823529df8 ("seq: port_info: add object to represent information of port for ALSA Sequencer")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/seq/port-info.h
@@ -16,4 +16,6 @@ struct _ALSASeqPortInfoClass {
 
 ALSASeqPortInfo *alsaseq_port_info_new();
 
+G_END_DECLS
+
 #endif