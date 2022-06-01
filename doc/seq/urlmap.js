2cda0ee6b150fc61ed8ad0a7ac002348cdb79b2c
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-06-01 11:35:44 +0900

    doc: seq: fix URL to document for ALSATimer
    Fixes: ed3138613655 ("doc: change URL for help pages")
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- doc/seq/urlmap.js
+++ doc/seq/urlmap.js
@@ -1,5 +1,5 @@
 baseURLs = [
     [ 'GLib', 'https://docs.gtk.org/glib/' ],
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
-    [ 'ALSATimer', 'https://alsa-project.github.io/alsa-gobject-docs/alsatimer/' ]
+    [ 'ALSATimer', 'https://alsa-project.github.io/gobject-introspection-docs/alsatimer/' ]
 ]

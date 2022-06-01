alsa-project/alsa-gobject

commit ed31386136558279b2ba324544d89b5eea3a8196

doc: change URL for help pages
After discussion about help pages for software under gobject-introspection
team of alsa-project organization in github.com, new repository is used
for the new URL to aggregate them.

This commit changes URL for the help pages and adds minor integration.

Reference: https://lore.kernel.org/alsa-devel/YmagC3V3t0k84k9A@workstation/
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

doc/seq/alsaseq.toml.in
@@ -3,9 +3,12 @@ version = "@VERSION@"
 description = "alsaseq"
 authors = "Takashi Sakamoto"
 license = "LGPL-3.0-or-later"
-website_url = "https://alsa-project.github.io/alsa-gobject-docs/alsaseq/"
+website_url = "https://alsa-project.github.io/gobject-introspection-docs/alsaseq/"
+docs_url = "https://alsa-project.github.io/gobject-introspection-docs/alsaseq/"
 browse_url = "https://github.com/alsa-project/alsa-gobject"
 repository_url = "https://github.com/alsa-project/alsa-gobject.git"
+devhelp=true
+search_index=true
 
 dependencies = [
   "GLib-2.0",

doc/ctl/alsactl.toml.in
@@ -3,9 +3,12 @@ version = "@VERSION@"
 description = "alsactl"
 authors = "Takashi Sakamoto"
 license = "LGPL-3.0-or-later"
-website_url = "https://alsa-project.github.io/alsa-gobject-docs/alsactl/"
+website_url = "https://alsa-project.github.io/gobject-introspection-docs/alsactl/"
+doc_url = "https://alsa-project.github.io/gobject-introspection-docs/alsactl/"
 browse_url = "https://github.com/alsa-project/alsa-gobject"
 repository_url = "https://github.com/alsa-project/alsa-gobject.git"
+devhelp=true
+search_index=true
 
 dependencies = [
   "GLib-2.0",

doc/hwdep/alsahwdep.toml.in
@@ -3,9 +3,12 @@ version = "@VERSION@"
 description = "alsahwdep"
 authors = "Takashi Sakamoto"
 license = "LGPL-3.0-or-later"
-website_url = "https://alsa-project.github.io/alsa-gobject-docs/alsahwdep/"
+website_url = "https://alsa-project.github.io/gobject-introspection-docs/alsahwdep/"
+docs_url = "https://alsa-project.github.io/gobject-introspection-docs/alsahwdep/"
 browse_url = "https://github.com/alsa-project/alsa-gobject"
 repository_url = "https://github.com/alsa-project/alsa-gobject.git"
+devhelp=true
+search_index=true
 
 dependencies = [
   "GLib-2.0",

doc/rawmidi/alsarawmidi.toml.in
@@ -3,9 +3,12 @@ version = "@VERSION@"
 description = "alsarawmidi"
 authors = "Takashi Sakamoto"
 license = "LGPL-3.0-or-later"
-website_url = "https://alsa-project.github.io/alsa-gobject-docs/alsarawmidi/"
+website_url = "https://alsa-project.github.io/gobject-introspection-docs/alsarawmidi/"
+docs_url = "https://alsa-project.github.io/gobject-introspection-docs/alsarawmidi/"
 browse_url = "https://github.com/alsa-project/alsa-gobject"
 repository_url = "https://github.com/alsa-project/alsa-gobject.git"
+devhelp=true
+search_index=true
 
 dependencies = [
   "GLib-2.0",

doc/timer/alsatimer.toml.in
@@ -3,9 +3,12 @@ version = "@VERSION@"
 description = "alsatimer"
 authors = "Takashi Sakamoto"
 license = "LGPL-3.0-or-later"
-website_url = "https://alsa-project.github.io/alsa-gobject-docs/alsatimer/"
+website_url = "https://alsa-project.github.io/gobject-introspection-docs/alsatimer/"
+docs_url = "https://alsa-project.github.io/gobject-introspection-docs/alsatimer/"
 browse_url = "https://github.com/alsa-project/alsa-gobject"
 repository_url = "https://github.com/alsa-project/alsa-gobject.git"
+devhelp=true
+search_index=true
 
 dependencies = [
   "GLib-2.0",

commit 2cda0ee6b150fc61ed8ad0a7ac002348cdb79b2c

doc: seq: fix URL to document for ALSATimer
Fixes: ed3138613655 ("doc: change URL for help pages")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

doc/seq/alsaseq.toml.in
@@ -29,7 +29,7 @@ docs_url = "https://docs.gtk.org/gobject/"
 [dependencies."ALSATimer-0.0"]
 name = "ALSATimer"
 description = "GObject Introspection library with timer API in asound.h"
-docs_url = "https://alsa-project.github.io/alsa-gobject-docs/alsatimer/"
+docs_url = "https://alsa-project.github.io/gobject-introspection-docs/alsatimer/"
 
 [source-location]
 base_url = "https://github.com/alsa-project/alsa-gobject/blob/master/"

doc/seq/urlmap.js
@@ -1,5 +1,5 @@
 baseURLs = [
     [ 'GLib', 'https://docs.gtk.org/glib/' ],
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
-    [ 'ALSATimer', 'https://alsa-project.github.io/alsa-gobject-docs/alsatimer/' ]
+    [ 'ALSATimer', 'https://alsa-project.github.io/gobject-introspection-docs/alsatimer/' ]
 ]