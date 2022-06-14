21c325c6c2d113ec57a27cf979bea2e22a2b7c87
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-06-14 19:23:21 +0900

    seq: remove-filter: remove unused error arguments
    ALSASeq.RemoveFilter class has some constructors according to
    the filter condition. These are defined as throw function, however error
    argument is not used at all.
    
    This commit removes the unused argument.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/seq/remove-filter.h
+++ src/seq/remove-filter.h
@@ -14,33 +14,30 @@ GType alsaseq_remove_filter_get_type() G_GNUC_CONST;
 
 ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_dest_addr(
                                 ALSASeqRemoveFilterFlag inout, guint8 queue_id,
-                                ALSASeqAddr *dest, GError **error);
+                                ALSASeqAddr *dest);
 
 ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_note_channel(
                                 ALSASeqRemoveFilterFlag inout, guint8 queue_id,
-                                guint8 channel, GError **error);
+                                guint8 channel);
 
 ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_event_type(
                                 ALSASeqRemoveFilterFlag inout, guint8 queue_id,
-                                ALSASeqEventType ev_type, GError **error);
+                                ALSASeqEventType ev_type);
 
 ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_note(
-                                ALSASeqRemoveFilterFlag inout, guint8 queue_id,
-                                GError **error);
+                                ALSASeqRemoveFilterFlag inout, guint8 queue_id);
 
 ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_tag(
                                 ALSASeqRemoveFilterFlag inout, guint8 queue_id,
-                                gint8 tag, GError **error);
+                                gint8 tag);
 
 ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_tick_time(
                                 ALSASeqRemoveFilterFlag inout, guint8 queue_id,
-                                gint32 tick_time, gboolean after,
-                                GError **error);
+                                gint32 tick_time, gboolean after);
 
 ALSASeqRemoveFilter *alsaseq_remove_filter_new_with_real_time(
                                 ALSASeqRemoveFilterFlag inout, guint8 queue_id,
-                                gint32 tv_sec, guint32 tv_nsec, gboolean after,
-                                GError **error);
+                                gint32 tv_sec, guint32 tv_nsec, gboolean after);
 
 G_END_DECLS
 
