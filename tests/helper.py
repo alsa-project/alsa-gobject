20944c753d5ab8b5b9d753b20937c13a4f206e17
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-06-19 20:38:51 +0900

    tests: test virtual functions in object
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- tests/helper.py
+++ tests/helper.py
@@ -1,5 +1,5 @@
 def test_object(target_type: object, props: tuple[str], methods: tuple[str],
-                signals: tuple[str]) -> bool:
+                vmethods: tuple[str], signals: tuple[str]) -> bool:
     # All of available methods are put into the list of attribute.
     for method in methods:
         if not hasattr(target_type, method):
@@ -9,6 +9,7 @@ def test_object(target_type: object, props: tuple[str], methods: tuple[str],
     # The properties, virtual methods, and signals in interface are not put
     # into the list of attribute in object implementing the interface.
     prop_labels = []
+    vmethod_labels = []
     signal_labels = []
 
     # The  gi.ObjectInfo and gi.InterfaceInfo keeps them. Let's traverse them.
@@ -16,12 +17,18 @@ def test_object(target_type: object, props: tuple[str], methods: tuple[str],
         if hasattr(info, '__info__'):
             for prop in info.__info__.get_properties():
                 prop_labels.append(prop.get_name())
+            for vfunc in info.__info__.get_vfuncs():
+                vmethod_labels.append('do_' + vfunc.get_name())
             for signal in info.__info__.get_signals():
                 signal_labels.append(signal.get_name())
 
     for prop in props:
         if prop not in prop_labels:
             print('Property {0} is not produced.'.format(prop))
+            return False
+    for vmethod in vmethods:
+        if vmethod not in vmethod_labels:
+            print('Vmethod {0} is not produced.'.format(vmethod))
             return False
     for signal in signals:
         if signal not in signal_labels:
