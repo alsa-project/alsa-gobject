alsa-project/alsa-gobject

commit cc92f5ca9478b0785d8286affb7d367eaec2fe94

utils: add utility to compute integer value from string literal
The conversion between string literal and integer is required in some
includes libraries.

This commit adds utility for it.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/utils/string.c
@@ -1,0 +1,18 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include <stdlib.h>
+#include <errno.h>
+
+long long_from_string(const char *literal, long *number)
+{
+    long val;
+    char *endptr;
+
+    errno = 0;
+    val = strtol(literal, &endptr, 10);
+    if (errno > 0)
+        return -errno;
+    if (!endptr || endptr == literal || *endptr != 0)
+        return -EINVAL;
+    *number = val;
+    return 0;
+}

src/ctl/query.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "privates.h"
 
+#include <utils.h>
+
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
@@ -153,12 +155,9 @@ void alsactl_get_card_id_list(guint **entries, gsize *entry_count,
         if (dev != NULL) {
             const char *sysnum = udev_device_get_sysnum(dev);
             long val;
-            char *endptr;
 
-            errno = 0;
-            val = strtol(sysnum, &endptr, 10);
-            if (errno == 0 && *endptr == '\0' && val >= 0) {
-                (*entries)[index] = (guint)val;
+            if (!long_from_string(sysnum, &val)) {
+                (*entries)[index] = val;
                 ++index;
             }
             udev_device_unref(dev);

src/hwdep/query.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "privates.h"
 
+#include <utils.h>
+
 #include <stdio.h>
 #include <errno.h>
 #include <stdbool.h>
@@ -183,11 +185,8 @@ void alsahwdep_get_device_id_list(guint card_id, guint **entries,
         if (dev != NULL) {
             const char *sysnum = udev_device_get_sysnum(dev);
             long val;
-            char *endptr;
 
-            errno = 0;
-            val = strtol(sysnum, &endptr, 10);
-            if (errno == 0 && *endptr == '\0' && val >= 0) {
+            if (!long_from_string(sysnum, &val)) {
                 (*entries)[index] = (guint)val;
                 ++index;
             }

src/rawmidi/query.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "privates.h"
 
+#include <utils.h>
+
 #include <stdio.h>
 #include <errno.h>
 #include <stdbool.h>
@@ -183,11 +185,8 @@ void alsarawmidi_get_device_id_list(guint card_id, guint **entries,
         if (dev != NULL) {
             const char *sysnum = udev_device_get_sysnum(dev);
             long val;
-            char *endptr;
 
-            errno = 0;
-            val = strtol(sysnum, &endptr, 10);
-            if (errno == 0 && *endptr == '\0' && val >= 0) {
+            if (!long_from_string(sysnum, &val)) {
                 (*entries)[index] = (guint)val;
                 ++index;
             }

src/timer/query.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "privates.h"
 
+#include <utils.h>
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <errno.h>
@@ -305,7 +307,7 @@ static void timer_get_node_param_value(const char *param_name, char *buf,
     int fd;
     ssize_t len;
     long v;
-    char *term;
+    int err;
 
     snprintf(literal, sizeof(literal), SYSFS_SND_TIMER_NODE "parameters/%s",
              param_name);
@@ -322,11 +324,9 @@ static void timer_get_node_param_value(const char *param_name, char *buf,
         goto end;
     }
 
-    v = strtol(buf, &term, 10);
-    if (errno > 0)
-        generate_file_error(error, errno, "strtol()");
-    else if (*term != '\n')
-        generate_file_error(error, EIO, "strtol()");
+    err = long_from_string(buf, &v);
+    if (err < 0)
+        generate_file_error(error, -err, "Fail to parse snd_timer module parameter as integer value");
     else
         *val = (int)v;
 end:

src/utils/meson.build
@@ -3,6 +3,7 @@ headers = [
 ]
 
 sources = [
+  'string.c',
 ]
 
 dependencies = [

src/utils/utils.h
@@ -2,4 +2,6 @@
 #ifndef __ALSA_GOBJECT_UTILS_H__
 #define __ALSA_GOBJECT_UTILS_H__
 
+long long_from_string(const char *literal, long *number);
+
 #endif

commit 3ebe00cf51a0af27977e969d0eca31f3df9dec32

utils: successfully parse the string teminated by '\n'
It requires to parse the literal from sysfs node.

Fixes: cc92f5ca9478 ("utils: add utility to compute integer value from string literal")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/utils/string.c
@@ -13,7 +13,7 @@ long long_from_string(const char *literal, long *number)
     val = strtol(literal, &endptr, 10);
     if (errno > 0)
         return -errno;
-    if (!endptr || endptr == literal || *endptr != 0)
+    if (!endptr || endptr == literal || (*endptr != 0 && *endptr != '\n'))
         return -EINVAL;
     *number = val;
     return 0;